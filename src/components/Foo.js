5e30ec2bc4d1f425dc1a89a2502af3e62cfb69d1
Tommi <mato12@hotmail.fi>
2018-10-08 17:51:24 +0300

    Simple theme

--- src/components/Foo.js
+++ src/components/Foo.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+import styled from 'styled-components';
+
+const StyledButton = styled(Button)`
+  background-color: ${props => props.theme.color};
+`;
 
 const Foo = ({ getUsers, users }) => (
   <div className="Foo">
     <header className="Foo-header">
       <p>FOO!!!!</p>
-      <Button onClick={() => getUsers()}>getUsers</Button>
+      <StyledButton onClick={() => getUsers()}>getUsers</StyledButton>
       {users.map(user => (
         <div key={user.id}>{user.name}</div>
       ))}
--- src/index.js
+++ src/index.js
@@ -6,19 +6,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 import configureStore from './config/configureStore';
+import ThemeProvider from './components/ThemeProvider';
 import Foo from './components/containers/FooContainer';
 import App from './components/App';
 
-import 'open-city-design/src/scss/main.scss';
-
 const Root = () => (
   <Provider store={configureStore()}>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route exact path="/foo" component={Foo} />
-      </Switch>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route exact path="/foo" component={Foo} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   </Provider>
 );
 
