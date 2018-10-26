ed01c9b8a76fb4f61d693cb52c6744799143230a
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-10-26 17:04:45 +0300

    Fixed tranlsation keys for forms

--- src/components/forms/fragments/FullName.js
+++ src/components/forms/fragments/FullName.js
@@ -8,10 +8,10 @@ import type { FormFragmentProps } from '../../../types/form';
 export default ({ prefix }: FormFragmentProps) => (
   <Row>
     <Col sm={4}>
-      <Text name={`${prefix}.first_name`} label={`form.${prefix}.first_name.label`} required />
+      <Text name={`${prefix}.first_name`} label="form.private_person.first_name.label" required />
     </Col>
     <Col sm={4}>
-      <Text name={`${prefix}.last_name`} label={`form.${prefix}.last_name.label`} required />
+      <Text name={`${prefix}.last_name`} label="form.private_person.last_name.label" required />
     </Col>
   </Row>
 );
