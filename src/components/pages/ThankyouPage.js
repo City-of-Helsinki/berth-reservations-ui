570f7836f62d4ce8829b418d2ec72994beb689e5
Artur Gajewski <info@arturgajewski.com>
2018-10-28 13:48:05 +0200

    Thank you -page (#40)

--- src/components/pages/ThankyouPage.js
+++ src/components/pages/ThankyouPage.js
@@ -1,7 +1,29 @@
 // @flow
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import styled from 'styled-components';
+import { FormattedMessage } from 'react-intl';
 import Layout from './Layout';
+import ThankYouLegend from '../ThankYouLegend';
 
-const BerthPage = () => <Layout>Thanks!</Layout>;
+const ButtonSection = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 1.5em;
+`;
 
-export default BerthPage;
+const ThankYouPage = () => (
+  <Layout>
+    <ThankYouLegend />
+    <ButtonSection>
+      <Link to="/">
+        <Button type="button">
+          <FormattedMessage tagName="span" id="site.buttons.back_to_frontpage" />
+        </Button>
+      </Link>
+    </ButtonSection>
+  </Layout>
+);
+
+export default ThankYouPage;
