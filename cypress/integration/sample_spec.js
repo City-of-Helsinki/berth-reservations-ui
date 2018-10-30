aa720da102d8626da4f7395ea66f5e7e9ee2fce4
Tommi <mato12@hotmail.fi>
2018-10-30 11:27:34 +0200

    Add eslint rules for Cypress

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,8 +1,6 @@
-// prettier-ignore
-describe('My First Test', function() {
-  it('finds the content "type"', function() {
-    cy.visit('http://localhost:3000')
-    cy.get('input[name="registered_boat.register_number"]')
-      .type('12345');
+describe('My First Test', () => {
+  it('finds the content "type"', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('input[name="registered_boat.register_number"]').type('12345');
   });
 });
