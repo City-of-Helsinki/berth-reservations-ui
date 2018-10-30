98a5904a9b5bddc40b42f56daf0b6cfb0bbec285
Tommi <mato12@hotmail.fi>
2018-10-30 11:26:03 +0200

    use attr selector instead id

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,7 +2,7 @@
 describe('My First Test', function() {
   it('finds the content "type"', function() {
     cy.visit('http://localhost:3000')
-    cy.get('#form.registered_boat.register_number')
+    cy.get('input[name="registered_boat.register_number"]')
       .type('12345');
   });
 });
