d8749811296e1bf09bbecab8ece7bd441f1152e1
Artur Gajewski <info@arturgajewski.com>
2018-10-30 09:18:07 +0200

    Localize error messages (#41)

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -1,4 +1,5 @@
-export const mustBePresent = value => (value ? undefined : 'Required');
-export const mustBeNumber = value => (isNaN(value) ? 'Must be a number' : undefined);
+export const mustBePresent = value => (value ? undefined : 'validation.message.required');
+export const mustBeNumber = value =>
+  isNaN(value) ? 'validation.message.must_be_number' : undefined;
 
 export default (...fns) => x => fns.reduce((v, f) => (f ? v || f(x) : v), undefined);
