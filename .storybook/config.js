e8558fc79bd9726ab3214bb101ecc743a06d2e23
Tommi <mato12@hotmail.fi>
2018-10-31 09:59:46 +0200

    Storybook configs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 
-import ThemeProvider from '../src/components/ThemeProvider';
-import IntlProvider from '../src/components/IntlProvider';
+import messages from '../src/config/translations';
+import theme from '../src/config/theme';
+
+import { ThemeProvider } from 'styled-components';
+import { IntlProvider } from 'react-intl';
+
+const locale = 'fi';
 
 function loadStories() {
   addDecorator(stories => (
-    <IntlProvider locale="fi">
-      <ThemeProvider>{stories()}</ThemeProvider>
+    <IntlProvider locale={locale} key={locale} messages={messages[locale]}>
+      <ThemeProvider theme={theme}>{stories()}</ThemeProvider>
     </IntlProvider>
   ));
   const req = require.context('../stories', true, /story\.js$/);
