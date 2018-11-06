f1247533750450e8ade77353b9ecca8527ba9197
Tommi <mato12@hotmail.fi>
2018-11-06 20:40:02 +0200

    first impression

--- api/index.js
+++ api/index.js
@@ -3,8 +3,9 @@ const cors = require('cors');
 const jsf = require('json-schema-faker');
 const faker = require('faker');
 const schemas = require('./schemas');
+const boatTypes = require('./boatTypes');
 
-require('dotenv').config({ path: '.env.local' });
+require('dotenv').config({ path: '.env.development.local' });
 
 jsf.extend('faker', () => faker);
 
@@ -15,5 +16,17 @@ app.use(cors());
 Object.entries(schemas).forEach(([resource, schema]) => {
   app.get(`/api/${resource}`, (req, res) => res.json(jsf.generate(schema)));
 });
+
+app.get(`/api/boat-types`, (req, res) =>
+  res.json({
+    count: Object.keys(boatTypes).length,
+    next: null,
+    previous: null,
+    results: Object.entries(boatTypes).map(([identifier, name]) => ({
+      identifier,
+      name
+    }))
+  })
+);
 
 app.listen(API_PORT, () => console.log(`API running on port ${API_PORT}!`));
--- api/schemas/berths.js
+++ api/schemas/berths.js
@@ -1,66 +1,116 @@
-const array = (items, minItems, maxItems) => ({
-  type: 'array',
-  minItems,
-  maxItems,
-  items
-});
+const utils = require('../utils');
 
-const object = properties => ({
-  type: 'object',
-  properties,
-  required: Object.keys(properties)
-});
-
-const fakedString = faked => ({
-  type: 'string',
-  faker: faked
-});
-
-const fakedInteger = faked => ({
-  type: 'integer',
-  faker: faked
-});
-const integer = (minimum, maximum) => ({
-  type: 'integer',
-  minimum,
-  maximum
-});
-
-const boolean = () => ({
-  type: 'boolean'
-});
-
-const fakedMultilingual = faked =>
-  object({
-    fi: fakedString(faked),
-    sv: fakedString(faked)
-  });
-
-module.exports = array(
-  object({
-    name: fakedMultilingual('address.streetName'),
-    street_address: fakedMultilingual('address.streetAddress'),
-    municipality: fakedMultilingual('address.city'),
-    zip_code: fakedString('address.zipCode'),
-    phone: fakedString('phone.phoneNumber'),
-    email: fakedString('internet.email'),
-    www_url: fakedString('internet.url'),
-    location: object({
-      type: fakedString('address.city'),
-      coordinates: array([fakedInteger('address.latitude'), fakedInteger('address.longitude')])
+module.exports = utils.array(
+  utils.object({
+    identifier: utils.fakedString('random.uuid'),
+    suitable_boat_types: utils.fakedBoatTypes(),
+    name: utils.fakedMultilingual('address.streetName'),
+    street_address: utils.fakedMultilingual('address.streetAddress'),
+    municipality: utils.fakedMultilingual('address.city'),
+    zip_code: utils.fakedString('address.zipCode'),
+    phone: utils.fakedString('phone.phoneNumber'),
+    email: utils.fakedString('internet.email'),
+    www_url: utils.fakedString('internet.url'),
+    location: utils.object({
+      type: utils.fakedString('address.city'),
+      coordinates: utils.array([
+        utils.fakedInteger('address.latitude'),
+        utils.fakedInteger('address.longitude')
+      ])
     }),
-    image_file: fakedString('image.imageUrl'),
-    image_link: fakedString('internet.url'),
-    mooring: boolean(),
-    electricity: boolean(),
-    water: boolean(),
-    waste_collection: boolean(),
-    gate: boolean(),
-    lighting: boolean(),
-    number_of_places: integer(10, 50),
-    maximum_depth: integer(100, 1000),
-    maximum_width: integer(100, 1000)
+    image: utils.fakedString('image.imageUrl'),
+    mooring: utils.boolean(),
+    electricity: utils.boolean(),
+    water: utils.boolean(),
+    waste_collection: utils.boolean(),
+    gate: utils.boolean(),
+    lighting: utils.boolean(),
+    number_of_places: utils.integer(10, 50),
+    maximum_depth: utils.integer(100, 1000),
+    maximum_width: utils.integer(100, 1000)
   }),
   20,
   30
 );
+
+/*
+  "suitable_boat_types": [
+      "dinghy",
+      "rowing_boat",
+      "motorboat_outboard"
+  ],
+  "municipality": {
+      "fi": "Helsinki",
+      "sv": "Helsingfors"
+  },
+  "image": "https://www.hel.fi/wps/wcm/connect/1a348a75-844a-4611-9f40-1812c6101533/1/ruoholahden_venesatama_53.jpg?MOD=AJPERES&CVID=",
+  "identifier": "test",
+  "zip_code": null,
+  "phone": null,
+  "email": null,
+  "www_url": null,
+  "mooring": false,
+  "electricity": false,
+  "water": false,
+  "waste_collection": false,
+  "gate": false,
+  "lighting": false,
+  "number_of_places": null,
+  "maximum_width": null,
+  "maximum_length": null,
+  "maximum_depth": null,
+  "location": {
+      "type": "Point",
+      "coordinates": [
+          25.1677895,
+          60.2528454
+      ]
+  },
+  "name": {
+      "fi": "Airoranta"
+  },
+  "street_address": {
+      "fi": "testikatu 1"
+  }
+},
+{
+  "suitable_boat_types": [
+      "dinghy",
+      "rowing_boat",
+      "motorboat_outboard"
+  ],
+  "municipality": {
+      "fi": "Helsinki",
+      "sv": "Helsingfors"
+  },
+  "image": "https://www.hel.fi/wps/wcm/connect/1a348a75-844a-4611-9f40-1812c6101533/1/ruoholahden_venesatama_53.jpg?MOD=AJPERES&CVID=",
+  "identifier": "test",
+  "zip_code": null,
+  "phone": null,
+  "email": null,
+  "www_url": null,
+  "mooring": false,
+  "electricity": false,
+  "water": false,
+  "waste_collection": false,
+  "gate": false,
+  "lighting": false,
+  "number_of_places": null,
+  "maximum_width": null,
+  "maximum_length": null,
+  "maximum_depth": null,
+  "location": {
+      "type": "Point",
+      "coordinates": [
+          25.1677895,
+          60.2528454
+      ]
+  },
+  "name": {
+      "fi": "Vuosaaren satama"
+  },
+  "street_address": {
+      "fi": "testikatu 1"
+  }
+}
+*/
