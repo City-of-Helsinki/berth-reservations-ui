4143a365afd086662e2b94e2e15ae18523690166
Tommi <mato12@hotmail.fi>
2018-11-12 13:41:52 +0200

    WIP

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -1,8 +1,19 @@
 // @flow
-export const mustBePresent = (value: string): void | string =>
+
+export const mustBePresent = (value: string | number): void | string =>
   value ? undefined : 'validation.message.required';
 export const mustBeNumber = (value: string | number): void | string =>
   Number.isNaN(value) ? 'validation.message.must_be_number' : undefined;
 
-export default (...fns: Array<Function | null>) => (x: string | number) =>
-  fns.reduce((v, f) => (f ? v || f(x) : v), undefined);
+export default (f1: Function | null, f2: Function | null) => (
+  x: string | number
+): void | string => {
+  let validated;
+  if (f1) {
+    validated = f1(x);
+  }
+  if (!validated && f2) {
+    validated = f2(x);
+  }
+  return validated;
+};
