7d91543f59c15deeb47af5a00fee9373fe459a8e
Tommi <mato12@hotmail.fi>
2018-11-13 09:43:53 +0200

    FUKE

---
+++ src/components/forms/Selects.js
@@ -0,0 +1,49 @@
+// @flow
+import React from 'react';
+import { injectIntl } from 'react-intl';
+import { Select } from './Fields';
+
+export const BigBoatTypeValue = 'big_boats';
+export const BoatType = injectIntl(({ prefix, noValidate = false, intl: { formatMessage } }) => (
+  <Select
+    noValidate={noValidate}
+    name={`${prefix}.type`}
+    label="form.registered.field.type.label"
+    required
+  >
+    <option>{formatMessage({ id: 'form.registered.field.type.placeholder' })}</option>
+    <option value={BigBoatTypeValue}>a</option>
+    <option>b</option>
+    <option>c</option>
+  </Select>
+));
+
+export const Propulsion = injectIntl(({ prefix, noValidate = false, intl: { formatMessage } }) => (
+  <Select
+    noValidate={noValidate}
+    name={`${prefix}.propulsion`}
+    label="form.big_ship.field.propulsion.label"
+    required
+  >
+    <option>{formatMessage({ id: 'form.big_ship.field.propulsion.placeholder' })}</option>
+    <option>a</option>
+    <option>b</option>
+    <option>c</option>
+  </Select>
+));
+
+export const HullMaterial = injectIntl(
+  ({ prefix, noValidate = false, intl: { formatMessage } }) => (
+    <Select
+      noValidate={noValidate}
+      name={`${prefix}.hull_material`}
+      label="form.big_ship.field.hull_material.label"
+      required
+    >
+      <option>{formatMessage({ id: 'form.big_ship.field.hull_material.placeholder' })}</option>
+      <option>a</option>
+      <option>b</option>
+      <option>c</option>
+    </Select>
+  )
+);
