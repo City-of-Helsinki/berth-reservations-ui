f14bb6ba15877ad01b9730bc6e0d052e4dacc859
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-11-14 14:08:48 +0200

    Add amount of found berths

--- src/components/berths/Berths.js
+++ src/components/berths/Berths.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import Berth from './Berth';
 
 const StyledBerth = styled(Berth)``;
@@ -11,8 +11,27 @@ const Wrapper = styled(Container)`
   }
 `;
 
+const StyledCol = styled(Col)`
+  padding: 0em;
+`;
+
+const BerthCount = styled.div`
+  width: 100%;
+  margin-bottom: 2em;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-weight: 500;
+  border-top: 2px solid black;
+  border-bottom: 2px solid black;
+`;
+
 export default ({ berths, onClick, selected }) => (
   <Wrapper>
+    <Row>
+      <StyledCol xs={12}>
+        <BerthCount>{berths.size} hakuehdot täyttävää satamaa</BerthCount>
+      </StyledCol>
+    </Row>
     {berths.map(berth => (
       <StyledBerth
         key={berth.identifier}
