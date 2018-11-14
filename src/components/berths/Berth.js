da7f3d8934771f97454147f2c1480a37438adbf1
Artur Gajewski <info@arturgajewski.com>
2018-11-14 15:20:39 +0200

    Add amount of found berths (#72)
    * Add amount of found berths
    
    * Restyle berth row

--- src/components/berths/Berth.js
+++ src/components/berths/Berth.js
@@ -15,6 +15,7 @@ const Details = styled.div`
 
   flex: 1 0 25%;
 `;
+
 const DetailsWrapper = styled.div`
   display: flex;
   flex-direction: row;
@@ -22,13 +23,6 @@ const DetailsWrapper = styled.div`
   flex-wrap: wrap;
   height: 100%;
   padding: 1em 0;
-`;
-
-const StyledRow = styled(Row)`
-  background-color: ${props => props.theme.helLight};
-  div:first-child {
-    padding-left: 0px;
-  }
 `;
 
 const DetailsIcon = styled(Icon).attrs({
@@ -41,6 +35,7 @@ const DetailsValue = styled.span`
   line-height: 42px;
   font-size: 24px;
 `;
+
 const DetailsTitle = styled.span``;
 
 const BerthImage = styled.img`
@@ -65,7 +60,7 @@ const SummaryWrapper = styled.div`
   }
   a {
     font-size: 12px;
-    color: #000;
+    color: black;
   }
 `;
 
@@ -84,6 +79,10 @@ const WebsiteLink = styled.a`
   margin-top: 1em;
 `;
 
+const StyledDiv = styled.div`
+  background-color: ${props => props.theme.helLight};
+`;
+
 type Props = {
   berth: Berth,
   className: string,
@@ -92,85 +91,90 @@ type Props = {
 };
 
 export default ({ berth, className, onClick, selected }: Props) => (
-  <StyledRow className={className}>
-    <Col xs={3}>
-      <BerthImage src={berth.image_file} alt={berth.name.fi} />
-    </Col>
-    <Col xs={4}>
-      <SummaryWrapper>
-        <strong>{berth.name.fi}</strong>
+  <Row className={className}>
+    <Col xs={12}>
+      <StyledDiv>
+        <Row>
+          <Col xs={3}>
+            <BerthImage src={berth.image_file} alt={berth.name.fi} />
+          </Col>
+          <Col xs={4}>
+            <SummaryWrapper>
+              <strong>{berth.name.fi}</strong>
 
-        <BerthAddress>
-          {berth.street_address.fi}, {berth.zip_code} {berth.municipality.fi}
-        </BerthAddress>
+              <BerthAddress>
+                {berth.street_address.fi}, {berth.zip_code} {berth.municipality.fi}
+              </BerthAddress>
+              {selected ? (
+                <Button color="secondary" onClick={onClick}>
+                  <ButtonIcon name="check" width="1em" height="1em" />
+                  <FormattedMessage tagName="span" id="page.berths.selected" />
+                </Button>
+              ) : (
+                <Button outline primary="true" onClick={onClick}>
+                  + <FormattedMessage tagName="span" id="page.berths.select" />
+                </Button>
+              )}
 
-        {selected ? (
-          <Button color="secondary" onClick={onClick}>
-            <ButtonIcon name="check" width="1em" height="1em" />
-            <FormattedMessage tagName="span" id="page.berths.selected" />
-          </Button>
-        ) : (
-          <Button outline primary="true" onClick={onClick}>
-            + <FormattedMessage tagName="span" id="page.berths.select" />
-          </Button>
-        )}
-
-        <WebsiteLink href={berth.www_url}>
-          <FormattedMessage tagName="span" id="page.berths.website" />
-        </WebsiteLink>
-      </SummaryWrapper>
+              <WebsiteLink href={berth.www_url}>
+                <FormattedMessage tagName="span" id="page.berths.website" />
+              </WebsiteLink>
+            </SummaryWrapper>
+          </Col>
+          <Col xs={5}>
+            <DetailsWrapper>
+              <Details available={true}>
+                <DetailsValue>{berth.number_of_places}</DetailsValue>
+                <DetailsTitle>
+                  <FormattedMessage tagName="span" id="page.berths.number_of_places" />
+                </DetailsTitle>
+              </Details>
+              <Details available={berth.mooring}>
+                <DetailsIcon name="pole" />
+                <DetailsTitle>
+                  <FormattedMessage tagName="span" id="page.berths.pole" />
+                </DetailsTitle>
+              </Details>
+              <Details available={berth.electricity}>
+                <DetailsIcon name="plug" />
+                <DetailsTitle>
+                  <FormattedMessage tagName="span" id="page.berths.electricity" />
+                </DetailsTitle>
+              </Details>
+              <Details available={berth.water}>
+                <DetailsIcon name="waterTap" />
+                <DetailsTitle>
+                  <FormattedMessage tagName="span" id="page.berths.water_tap" />
+                </DetailsTitle>
+              </Details>
+              <Details available={true}>
+                <DetailsValue>{berth.maximum_width}</DetailsValue>
+                <DetailsTitle>
+                  <FormattedMessage tagName="span" id="page.berths.maximum_width" />
+                </DetailsTitle>
+              </Details>
+              <Details available={berth.waste_collection}>
+                <DetailsIcon name="trash" />
+                <DetailsTitle>
+                  <FormattedMessage tagName="span" id="page.berths.waste_collection" />
+                </DetailsTitle>
+              </Details>
+              <Details available={berth.gate}>
+                <DetailsIcon name="fence" />
+                <DetailsTitle>
+                  <FormattedMessage tagName="span" id="page.berths.fence" />
+                </DetailsTitle>
+              </Details>
+              <Details available={berth.lighting}>
+                <DetailsIcon name="streetLight" />
+                <DetailsTitle>
+                  <FormattedMessage tagName="span" id="page.berths.lighting" />
+                </DetailsTitle>
+              </Details>
+            </DetailsWrapper>
+          </Col>
+        </Row>
+      </StyledDiv>
     </Col>
-    <Col xs={5}>
-      <DetailsWrapper>
-        <Details available={true}>
-          <DetailsValue>{berth.number_of_places}</DetailsValue>
-          <DetailsTitle>
-            <FormattedMessage tagName="span" id="page.berths.number_of_places" />
-          </DetailsTitle>
-        </Details>
-        <Details available={berth.mooring}>
-          <DetailsIcon name="pole" />
-          <DetailsTitle>
-            <FormattedMessage tagName="span" id="page.berths.pole" />
-          </DetailsTitle>
-        </Details>
-        <Details available={berth.electricity}>
-          <DetailsIcon name="plug" />
-          <DetailsTitle>
-            <FormattedMessage tagName="span" id="page.berths.electricity" />
-          </DetailsTitle>
-        </Details>
-        <Details available={berth.water}>
-          <DetailsIcon name="waterTap" />
-          <DetailsTitle>
-            <FormattedMessage tagName="span" id="page.berths.water_tap" />
-          </DetailsTitle>
-        </Details>
-        <Details available={true}>
-          <DetailsValue>{berth.maximum_width}</DetailsValue>
-          <DetailsTitle>
-            <FormattedMessage tagName="span" id="page.berths.maximum_width" />
-          </DetailsTitle>
-        </Details>
-        <Details available={berth.waste_collection}>
-          <DetailsIcon name="trash" />
-          <DetailsTitle>
-            <FormattedMessage tagName="span" id="page.berths.waste_collection" />
-          </DetailsTitle>
-        </Details>
-        <Details available={berth.gate}>
-          <DetailsIcon name="fence" />
-          <DetailsTitle>
-            <FormattedMessage tagName="span" id="page.berths.fence" />
-          </DetailsTitle>
-        </Details>
-        <Details available={berth.lighting}>
-          <DetailsIcon name="streetLight" />
-          <DetailsTitle>
-            <FormattedMessage tagName="span" id="page.berths.lighting" />
-          </DetailsTitle>
-        </Details>
-      </DetailsWrapper>
-    </Col>
-  </StyledRow>
+  </Row>
 );
--- src/components/berths/Berths.js
+++ src/components/berths/Berths.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import Berth from './Berth';
 
 const StyledBerth = styled(Berth)``;
@@ -11,8 +11,23 @@ const Wrapper = styled(Container)`
   }
 `;
 
+const BerthCount = styled.div`
+  width: 100%;
+  margin-bottom: 2em;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-weight: 500;
+  border-top: 2px solid black;
+  border-bottom: 2px solid black;
+`;
+
 export default ({ berths, onClick, selected }) => (
   <Wrapper>
+    <Row>
+      <Col xs={12}>
+        <BerthCount>{berths.size} hakuehdot täyttävää satamaa</BerthCount>
+      </Col>
+    </Row>
     {berths.map(berth => (
       <StyledBerth
         key={berth.identifier}
