548028fc348e4fe017c677adb61fc9ff411c27e7
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-11-21 09:59:18 +0200

    Add phone and email validators

--- src/components/forms/fragments/ContactDetails.js
+++ src/components/forms/fragments/ContactDetails.js
@@ -4,12 +4,14 @@ import { Row, Col } from 'reactstrap';
 import { Text } from '../Fields';
 
 import type { FormFragmentProps } from '../../../types/form';
+import { mustBePhoneNumber, mustBeEmail } from '../../../utils/formValidation';
 
 const ContactDetailsFragment = ({ prefix, noValidate = false }: FormFragmentProps) => (
   <Row>
     <Col sm={4}>
       <Text
         noValidate={noValidate}
+        validate={mustBePhoneNumber}
         name={`${prefix}.mobile_phone`}
         label="form.contact_details.field.mobile_phone.label"
         required
@@ -18,6 +20,7 @@ const ContactDetailsFragment = ({ prefix, noValidate = false }: FormFragmentProp
     <Col sm={4}>
       <Text
         noValidate={noValidate}
+        validate={mustBeEmail}
         name={`${prefix}.email`}
         label={`form.contact_details.field.email.label`}
         required
--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -2,8 +2,25 @@
 
 export const mustBePresent = (value: any): any =>
   value ? undefined : 'validation.message.required';
+
 export const mustBeNumber = (value: any): any =>
   Number.isNaN(value) ? 'validation.message.must_be_number' : undefined;
+
+export const mustBePhoneNumber = (value: any): any => {
+  const phoneRe = /^([0-9\(\)\s\+\-])+$/im;
+  if (phoneRe.test(value)) {
+    return undefined;
+  }
+  return 'validation.message.must_be_phone_number';
+};
+
+export const mustBeEmail = (value: any): any => {
+  const emailRe = /^.*\@.*\..*$/im;
+  if (emailRe.test(value)) {
+    return undefined;
+  }
+  return 'validation.message.must_be_email';
+};
 
 export default (f1: Function | null, f2: Function | null) => (x: any): any => {
   let validated;
