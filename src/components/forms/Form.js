909486aff237f0f6cc19b24c74bd806081b86059
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-11-21 13:58:13 +0200

    Removed unused import

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { type Node } from 'react';
-import { Form as FinalForm, Field } from 'react-final-form';
+import { Form as FinalForm } from 'react-final-form';
 import { Form as BootstrapForm } from 'reactstrap';
 
 type Props = {
