871d4513065bae9c1eedfefc4eb9a188cdbcce72
Tommi <mato12@hotmail.fi>
2018-12-11 13:52:04 +0200

    new tab selector

--- src/components/berths/TabSelector.js
+++ src/components/berths/TabSelector.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
+// @flow
+import React, { Component, type Node } from 'react';
 import styled from 'styled-components';
 import { StickyContainer, Sticky } from 'react-sticky';
 import responsive from '../../utils/responsive';
 
 const TabsWrapper = styled.div`
-  background-color: ${props => props.theme.helFog};
-  display: flex;
-  z-index: 1001;
-  justify-content: center;
+  background-color: ${props => props.theme.helLight};
   border-bottom: 4px solid white;
+  z-index: 1001;
 `;
+const TabsInnerWrapper = styled.div`
+  max-width: ${props => props.theme.maxWidth.xl};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-left: 15px;
+  margin-right: 15px;
+  margin: auto;
+  flex-direction: column-reverse;
+  ${responsive.sm`
+    flex-direction: row;
+  `}
+`;
+
 const TabButton = styled.button.attrs({
   type: 'button'
 })`
@@ -31,8 +46,18 @@ const Tabs = styled.div`
   margin-top: 3em;
 `;
 
-class TabSelector extends Component {
-  constructor(props) {
+type Props = {
+  children: Array<Node>,
+  progress: Function,
+  progressDisabled: boolean
+};
+
+type State = {
+  tab: number
+};
+
+class TabSelector extends Component<Props, State> {
+  constructor(props: Props) {
     super(props);
 
     this.state = {
@@ -40,7 +65,7 @@ class TabSelector extends Component {
     };
   }
 
-  selectTab = tab => {
+  selectTab = (tab: number) => {
     this.setState(() => ({ tab }));
   };
 
@@ -52,19 +77,30 @@ class TabSelector extends Component {
 
   render() {
     const { tab } = this.state;
-    const { children } = this.props;
+    const { children, progress, progressDisabled } = this.props;
 
+    // $FlowFixMe
     const headers = children.map(c => c.props.TabHeader);
     return (
       <StickyContainer>
         <Sticky>
           {({ style }) => (
             <TabsWrapper style={style}>
-              {headers.map((T, i) => (
-                <TabButton key={i} onClick={() => this.selectTab(i)} active={i === tab}>
-                  <T />
-                </TabButton>
-              ))}
+              <TabsInnerWrapper>
+                <div>
+                  {headers.map((T, i) => (
+                    <TabButton block key={i} onClick={() => this.selectTab(i)} active={i === tab}>
+                      <T />
+                    </TabButton>
+                  ))}
+                </div>
+                <div>
+                  <span>Sinulla on vielä maksimissaan 10 satamaa valittavissa</span>
+                  <button onClick={progress} disabled={progressDisabled}>
+                    JATKA
+                  </button>
+                </div>
+              </TabsInnerWrapper>
             </TabsWrapper>
           )}
         </Sticky>
