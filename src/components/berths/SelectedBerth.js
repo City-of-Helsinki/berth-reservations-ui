8e54ea23ef41f1adbd3af5b74f580660601128db
Tero Tikkanen <tero.tikkanen@gmail.com>
2019-01-10 23:51:43 +0200

    Revise berth selection list styles

--- src/components/berths/SelectedBerth.js
+++ src/components/berths/SelectedBerth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Button } from 'reactstrap';
 import styled from 'styled-components';
 import { injectIntl } from 'react-intl';
 
@@ -8,41 +8,55 @@ import responsive from '../../utils/responsive';
 import { getLocalizedText } from '../../utils/berths';
 import InvalidSelection from './InvalidSelection';
 
-const StyledRow = styled(Row)`
-  margin-top: 0.2em;
-  margin-bottom: 0.2em;
+const BerthRow = styled.div`
+  display: flex;
+  margin-bottom: 0.5em;
 `;
 
-const BerthName = styled(Col)`
+const BerthName = styled.div`
+  display: flex;
+  flex-grow: 1;
+  align-items: center;
   background-color: ${props =>
     props.errors === 'true' ? props.theme.helCoat : props.theme.helLight};
   color: ${props => (props.errors === 'true' ? props.theme.helWhite : props.theme.helGray)};
   font-size: 0.875em;
+  line-height: 1;
   padding: 0.3em;
   padding-left: 0.8em;
+  margin-right: 0.5em;
   ${responsive.sm`
-    font-size: 1.5em;
+    font-size: 1.2em;
   `}
+
+  span {
+    flex-grow: 1;
+  }
 `;
 
-const BerthOptions = styled(Col)`
+const BerthOptions = styled.div`
   display: flex;
   align-content: center;
 `;
 
-const StyledButton = styled.button`
-  background: none;
-  height: 100%;
-  border: 2px solid ${props => (props.disabled ? 'lightgray' : 'black')};
+const StyledButton = styled(Button)`
+  padding-left: 0;
+  padding-right: 0;
+  &:first-child {
+    margin-right: 6px;
+  }
+  &:hover {
+    background-color: ${props => props.theme.helFog};
+  }
+  ${responsive.sm`
+    padding-left:  0.75rem;
+    padding-right: 0.75rem;
+  `}
 `;
 
-const DeselectButton = styled.button`
-  background: none;
+const DeselectButton = styled(Button)`
   height: 100%;
   margin-left: 1em;
-  border: none;
-  float: right;
-  color: white;
 `;
 
 class SelectedBerth extends Component<any, any> {
@@ -61,43 +75,36 @@ class SelectedBerth extends Component<any, any> {
 
     const id = `tooltip_${berth.identifier}`;
     return (
-      <Container fluid>
-        <StyledRow>
-          <BerthName xs={8} md={9} errors={isValid.toString()}>
-            <DeselectButton type="button" onClick={() => deselectBerth(berth.identifier)}>
-              <Icon name="times" width="30px" />
-            </DeselectButton>
-            <span key={berth.identifier}>
-              {index + 1}. {getLocalizedText(berth.name, intl.locale)}
-            </span>
-            {!isValid && <InvalidSelection id={id} />}
-          </BerthName>
-          <BerthOptions xs={4} md={3}>
-            <Container fluid>
-              <Row>
-                <Col xs={12} sm={6}>
-                  <StyledButton
-                    type="button"
-                    onClick={() => moveUp(berth.identifier)}
-                    disabled={first}
-                  >
-                    <Icon name="angleUp" width="30px" color={first ? 'lightgray' : 'black'} />
-                  </StyledButton>
-                </Col>
-                <Col xs={12} sm={6}>
-                  <StyledButton
-                    type="button"
-                    onClick={() => moveDown(berth.identifier)}
-                    disabled={last}
-                  >
-                    <Icon name="angleDown" width="30px" color={last ? 'lightgray' : 'black'} />
-                  </StyledButton>
-                </Col>
-              </Row>
-            </Container>
-          </BerthOptions>
-        </StyledRow>
-      </Container>
+      <BerthRow>
+        <BerthName errors={isValid.toString()}>
+          <span key={berth.identifier}>
+            {index + 1}. {getLocalizedText(berth.name, intl.locale)}
+          </span>
+          <DeselectButton type="button" onClick={() => deselectBerth(berth.identifier)}>
+            <Icon name="times" width="30px" />
+          </DeselectButton>
+          {!isValid && <InvalidSelection id={id} />}
+        </BerthName>
+        <BerthOptions>
+          <StyledButton
+            outline
+            color="primary"
+            onClick={() => moveUp(berth.identifier)}
+            disabled={first}
+          >
+            <Icon name="angleUp" width="36px" color={first ? 'lightgray' : 'black'} />
+          </StyledButton>
+
+          <StyledButton
+            outline
+            color="primary"
+            onClick={() => moveDown(berth.identifier)}
+            disabled={last}
+          >
+            <Icon name="angleDown" width="36px" color={last ? 'lightgray' : 'black'} />
+          </StyledButton>
+        </BerthOptions>
+      </BerthRow>
     );
   }
 }
