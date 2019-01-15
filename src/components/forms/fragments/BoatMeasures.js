4e4078918666b5107638cf1a0f2643f66d975054
Artur Gajewski <info@arturgajewski.com>
2019-01-15 08:16:28 +0200

    Allow decimal with period and comman, validation error if negative (#144)

--- src/components/forms/fragments/BoatMeasures.js
+++ src/components/forms/fragments/BoatMeasures.js
@@ -6,60 +6,56 @@ import { Row, Col } from 'reactstrap';
 import { Number } from '../Fields';
 
 import type { FormFragmentProps } from '../../../types/form';
-import { mustBeNumber } from '../../../utils/formValidation';
+import { mustBePositiveNumber } from '../../../utils/formValidation';
 
 const RegisteredBoatFragment = ({ prefix, noValidate = false }: FormFragmentProps) => (
   <Row>
     <Col sm={3}>
       <Number
         noValidate={noValidate}
-        validate={mustBeNumber}
+        validate={mustBePositiveNumber}
         name={`${prefix}.width`}
         label="form.registered.field.width.label"
         placeholder="form.registered.field.width.placeholder"
         append="m"
         min="0"
-        parse={value => Math.max(0, parseFloat(value)).toString()}
         required
       />
     </Col>
     <Col sm={3}>
       <Number
         noValidate={noValidate}
-        validate={mustBeNumber}
+        validate={mustBePositiveNumber}
         name={`${prefix}.length`}
         label="form.registered.field.length.label"
         placeholder="form.registered.field.length.placeholder"
         append="m"
         min="0"
-        parse={value => Math.max(0, parseFloat(value)).toString()}
         required
       />
     </Col>
     <Col sm={3}>
       <Number
         noValidate={noValidate}
-        validate={mustBeNumber}
+        validate={mustBePositiveNumber}
         name={`${prefix}.draught`}
         label="form.registered.field.draught.label"
         placeholder="form.registered.field.draught.placeholder"
         append="m"
         min="0"
-        parse={value => Math.max(0, parseFloat(value)).toString()}
         required
       />
     </Col>
     <Col sm={3}>
       <Number
         noValidate={noValidate}
-        validate={mustBeNumber}
+        validate={mustBePositiveNumber}
         name={`${prefix}.weight`}
         step={100}
         label="form.registered.field.weight.label"
         placeholder="form.registered.field.weight.placeholder"
         append="kg"
         min="0"
-        parse={value => Math.max(0, parseFloat(value)).toString()}
         required
       />
     </Col>
