ddc74c76494235495b6291eed143aaa2946e190a
Tero Tikkanen <tero.tikkanen@gmail.com>
2019-01-16 00:01:10 +0200

    Fix map position

--- src/components/berths/BerthsOnMap.js
+++ src/components/berths/BerthsOnMap.js
@@ -2,16 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Map from '../Map';
 
-const MapContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-`;
-
 export default ({ onClick, selected, filtered, filteredNot }) => (
   <div>
-    <MapContainer>
-      <Map onClick={onClick} selected={selected} filtered={filtered} filteredNot={filteredNot} />
-    </MapContainer>
+    <Map onClick={onClick} selected={selected} filtered={filtered} filteredNot={filteredNot} />
   </div>
 );
