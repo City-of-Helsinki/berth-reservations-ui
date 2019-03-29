009a9623482970022f6ed670c520fccc0ddce227
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-03-29 14:42:35 +0200

    add declaration file for piwik-react-router package

---
+++ src/@types/piwik-react-router.d.ts
@@ -0,0 +1,15 @@
+declare module 'piwik-react-router' {
+  export default function PiwikReactRouter(options: {
+    url?: string;
+    siteId?: string;
+    enableLinkTracking?: boolean;
+    updateDocumentTitle?: boolean;
+    ignoreInitialVisit?: boolean;
+    trackErrorHandler?: Function;
+    injectScript?: boolean;
+    clientTrackerName?: string;
+    serverTrackerName?: string;
+  }): {
+    connectToHistory<H>(history: H): H;
+  };
+}
