f9d40acb56f6caee64d8e372a390c307ef82843a
Rasbin Rijal <rasbin.rijal@gmail.com>
2019-04-29 10:53:56 +0300

    Change input type for boat dimensions to number

--- src/components/forms/Fields.tsx
+++ src/components/forms/Fields.tsx
@@ -14,6 +14,6 @@ export const Text = mapNameAsId(injectIntl(Input('text')));
 export const Select = mapNameAsId(injectIntl(CustomInput('select', false)));
 export const Checkbox = mapNameAsId(injectIntl(CustomInput('checkbox', true)));
 export const Radio = mapNameAsId(injectIntl(CustomInput('radio', true)));
-export const Number = mapNameAsId(injectIntl(InputGroup('text')));
+export const Number = mapNameAsId(injectIntl(InputGroup('number')));
 export const MultiCheckbox = mapNameAsId(injectIntl(MultiInput('checkbox')));
 export const MultiRadio = mapNameAsId(injectIntl(MultiInput('radio')));
