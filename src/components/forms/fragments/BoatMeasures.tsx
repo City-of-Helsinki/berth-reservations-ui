96042a70bfc8ccb03658d40986da4b4ffadad638
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-05-08 22:34:29 +0300

    Add labels to form fragments

--- src/components/forms/fragments/BoatMeasures.tsx
+++ src/components/forms/fragments/BoatMeasures.tsx
@@ -1,58 +1,71 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import { Col, Row } from 'reactstrap';
 
 import { Number } from '../Fields';
 
 import { mustBePositiveNumber } from '../../../utils/formValidation';
 
-const RegisteredBoatFragment = () => (
-  <Row>
-    <Col sm={3}>
-      <Number
-        validate={mustBePositiveNumber}
-        name={`boatWidth`}
-        label="form.registered.field.width.label"
-        placeholder="form.registered.field.width.placeholder"
-        append="m"
-        min="0"
-        required
-      />
-    </Col>
-    <Col sm={3}>
-      <Number
-        validate={mustBePositiveNumber}
-        name={`boatLength`}
-        label="form.registered.field.length.label"
-        placeholder="form.registered.field.length.placeholder"
-        append="m"
-        min="0"
-        required
-      />
-    </Col>
-    <Col sm={3}>
-      <Number
-        validate={mustBePositiveNumber}
-        name={`boatDraught`}
-        label="form.registered.field.draught.label"
-        placeholder="form.registered.field.draught.placeholder"
-        append="m"
-        min="0"
-        required
-      />
-    </Col>
-    <Col sm={3}>
-      <Number
-        validate={mustBePositiveNumber}
-        name={`boatWeight`}
-        step={100}
-        label="form.registered.field.weight.label"
-        placeholder="form.registered.field.weight.placeholder"
-        append="kg"
-        min="0"
-        required
-      />
-    </Col>
-  </Row>
+interface Props {
+  showDraught?: boolean;
+  showWeight?: boolean;
+}
+
+const RegisteredBoatFragment = ({ showDraught, showWeight }: Props) => (
+  <>
+    <FormattedMessage tagName="h3" id="form.registered.header.measures" />
+    <Row>
+      <Col sm={3}>
+        <Number
+          validate={mustBePositiveNumber}
+          name={`boatWidth`}
+          label="form.registered.field.width.label"
+          placeholder="form.registered.field.width.placeholder"
+          append="m"
+          min="0"
+          required
+        />
+      </Col>
+      <Col sm={3}>
+        <Number
+          validate={mustBePositiveNumber}
+          name={`boatLength`}
+          label="form.registered.field.length.label"
+          placeholder="form.registered.field.length.placeholder"
+          append="m"
+          min="0"
+          required
+        />
+      </Col>
+      {showDraught && (
+        <Col sm={3}>
+          <Number
+            validate={mustBePositiveNumber}
+            name={`boatDraught`}
+            label="form.registered.field.draught.label"
+            placeholder="form.registered.field.draught.placeholder"
+            append="m"
+            min="0"
+            required
+          />
+        </Col>
+      )}
+      {showWeight && (
+        <Col sm={3}>
+          <Number
+            validate={mustBePositiveNumber}
+            name={`boatWeight`}
+            step={100}
+            label="form.registered.field.weight.label"
+            placeholder="form.registered.field.weight.placeholder"
+            append="kg"
+            min="0"
+            required
+          />
+        </Col>
+      )}
+    </Row>
+  </>
 );
 
 export default RegisteredBoatFragment;
--- src/components/forms/fragments/RegisteredBoatDetails.tsx
+++ src/components/forms/fragments/RegisteredBoatDetails.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import { Col, Row } from 'reactstrap';
+
 import { Text } from '../Fields';
 import { BoatType, WithBoatType } from '../Selects';
 
 const RegisteredBoatDetailsFragment = ({ boatTypes }: WithBoatType) => (
-  <Row>
-    <Col sm={6}>
-      <Text
-        name={`boatRegistrationNumber`}
-        label="form.registered.field.register_number.label"
-        placeholder="form.registered.field.register_number.placeholder"
-        required
-      />
-    </Col>
-    <Col sm={6}>
-      <BoatType required boatTypes={boatTypes} />
-    </Col>
-  </Row>
+  <>
+    <FormattedMessage tagName="h3" id="form.registered.header.title" />
+    <Row>
+      <Col sm={6}>
+        <Text
+          name={`boatRegistrationNumber`}
+          label="form.registered.field.register_number.label"
+          placeholder="form.registered.field.register_number.placeholder"
+          required
+        />
+      </Col>
+      <Col sm={6}>
+        <BoatType required boatTypes={boatTypes} />
+      </Col>
+    </Row>
+  </>
 );
 
 export default RegisteredBoatDetailsFragment;
