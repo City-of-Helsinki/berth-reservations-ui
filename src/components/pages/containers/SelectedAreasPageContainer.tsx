4a9ec95e54391818ad0b8fc002ff4254a367ff51
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-05-09 09:57:54 +0300

    Containers for winter areas pages

---
+++ src/components/pages/containers/SelectedAreasPageContainer.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import {
+  deselectWinterArea,
+  moveWinterAreaDown,
+  moveWinterAreaUp
+} from '../../../redux/actions/WinterAreaActions';
+import { withMatchParamsHandlers } from '../../../utils/container';
+import SelectedBerthPage from '../BerthPage/SelectedBerthPage';
+
+import { Store } from '../../../redux/types';
+import { SelectedServices } from '../../../types/services';
+import { Berths } from '../../berths/types';
+
+import { BOAT_TYPES_BERTHS_QUERY } from '../../../utils/graphql';
+import BoatsBerthsQuery from '../../common/BoatsBerthsQuery';
+
+interface Props {
+  selectedBerths: Berths;
+  selectedServices: SelectedServices;
+  deselectBerth: Function;
+  moveUp: Function;
+  moveDown: Function;
+  localePush: Function;
+  values: {};
+}
+
+const steps = [
+  {
+    key: 'winter_areas',
+    completed: true,
+    current: false,
+    linkTo: `winter_storage`
+  },
+  {
+    key: 'review_areas',
+    completed: false,
+    current: true,
+    linkTo: undefined
+  },
+  {
+    key: 'boat_information',
+    completed: false,
+    current: false,
+    linkTo: undefined
+  },
+  {
+    key: 'applicant',
+    completed: false,
+    current: false,
+    linkTo: undefined
+  },
+  {
+    key: 'send_application',
+    completed: false,
+    current: false,
+    linkTo: undefined
+  }
+];
+
+const UnconnectedSelectedBerthPage = (props: Props) => {
+  const moveToForm = async () => {
+    await props.localePush('/winter_form/registered_boat');
+  };
+
+  const handlePrevious = async () => {
+    await props.localePush('/winter_storage');
+  };
+
+  return (
+    <BoatsBerthsQuery query={BOAT_TYPES_BERTHS_QUERY}>
+      {({
+        // error, TODO: handle errors
+        data
+      }) => {
+        const boatTypes = data ? data.boatTypes : [];
+
+        return (
+          <SelectedBerthPage
+            handlePrevious={handlePrevious}
+            moveToForm={moveToForm}
+            boatTypes={boatTypes}
+            steps={steps}
+            {...props}
+          />
+        );
+      }}
+    </BoatsBerthsQuery>
+  );
+};
+
+export default compose<Props, {}>(
+  withMatchParamsHandlers,
+  connect(
+    (state: Store) => ({
+      selectedBerths: state.winterAreas.selectedWinterAreas,
+      selectedServices: state.winterAreas.selectedWinterServices,
+      values: state.forms.values
+    }),
+    {
+      deselectBerth: deselectWinterArea,
+      moveUp: moveWinterAreaUp,
+      moveDown: moveWinterAreaDown
+    }
+  )
+)(UnconnectedSelectedBerthPage);
---
+++ src/components/pages/containers/WinterBerthPageContainer.tsx
@@ -0,0 +1,151 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import { onSubmit } from '../../../redux/actions/FormActions';
+import {
+  deselectService,
+  deselectWinterArea,
+  selectService,
+  selectWinterArea
+} from '../../../redux/actions/WinterAreaActions';
+import { getBerths as getBerthsFromCache } from '../../../utils/berths';
+import { withMatchParamsHandlers } from '../../../utils/container';
+import { WINTER_AREAS_QUERY } from '../../../utils/graphql';
+import { IconNames } from '../../common/Icon';
+import WinterAreasQuery from '../../common/WinterAreasQuery';
+import BerthPage from '../BerthPage';
+
+import { Store } from '../../../redux/types';
+import { SelectedServices } from '../../../types/services';
+import { Berths as BerthsType } from '../../berths/types';
+
+interface Props {
+  initialValues: {};
+  filtered: BerthsType;
+  filteredNot: BerthsType;
+  selectedBerths: BerthsType;
+  selectedServices: SelectedServices;
+  selectBerth: Function;
+  deselectBerth: Function;
+  selectService: Function;
+  deselectService: Function;
+  onSubmit: Function;
+  localePush: Function;
+}
+
+const BerthPageContainer = (props: Props) => {
+  const steps = [
+    {
+      key: 'winter_areas',
+      completed: false,
+      current: true,
+      linkTo: undefined
+    },
+    {
+      key: 'review_areas',
+      completed: false,
+      current: false,
+      linkTo: undefined
+    },
+    {
+      key: 'boat_information',
+      completed: false,
+      current: false,
+      linkTo: undefined
+    },
+    {
+      key: 'applicant',
+      completed: false,
+      current: false,
+      linkTo: undefined
+    },
+    {
+      key: 'send_application',
+      completed: false,
+      current: false,
+      linkTo: undefined
+    }
+  ];
+
+  const services: Array<{
+    label: string;
+    value:
+      | 'electricity'
+      | 'water'
+      | 'gate'
+      | 'numberOfMarkedPlaces'
+      | 'repairArea'
+      | 'summerStorageForDockingEquipment'
+      | 'summerStorageForTrailers';
+    icon: IconNames;
+  }> = [
+    {
+      label: 'form.services.field.appointed_spaces.label',
+      value: 'numberOfMarkedPlaces',
+      icon: 'divided'
+    },
+    { label: 'form.services.field.water.label', value: 'water', icon: 'waterTap' },
+    { label: 'form.services.field.gate.label', value: 'gate', icon: 'fence' },
+    {
+      label: 'form.services.field.electricity.label',
+      value: 'electricity',
+      icon: 'plug'
+    },
+    {
+      label: 'form.services.field.storage_for_trailers.label',
+      value: 'summerStorageForTrailers',
+      icon: 'dollyEmpty'
+    },
+    {
+      label: 'form.services.field.storage_for_docking_equip.label',
+      value: 'summerStorageForDockingEquipment',
+      icon: 'trestle'
+    },
+    {
+      label: 'form.services.field.repair_area.label',
+      value: 'repairArea',
+      icon: 'tools'
+    }
+  ];
+
+  return (
+    <WinterAreasQuery query={WINTER_AREAS_QUERY}>
+      {({
+        // error, TODO: handle errors
+        data
+      }) => {
+        const winterAreas = getBerthsFromCache(data ? data.winterStorageAreas : null);
+        const boatTypes = data ? data.boatTypes : [];
+
+        return (
+          <BerthPage
+            {...props}
+            hero="winter"
+            berths={winterAreas}
+            boatTypes={boatTypes}
+            steps={steps}
+            services={services}
+          />
+        );
+      }}
+    </WinterAreasQuery>
+  );
+};
+
+export default compose<Props, {}>(
+  withMatchParamsHandlers,
+  connect(
+    (state: Store) => ({
+      initialValues: state.forms.values,
+      selectedBerths: state.winterAreas.selectedWinterAreas,
+      selectedServices: state.winterAreas.selectedWinterServices
+    }),
+    {
+      onSubmit,
+      selectService,
+      deselectService,
+      selectBerth: selectWinterArea,
+      deselectBerth: deselectWinterArea
+    }
+  )
+)(BerthPageContainer);
