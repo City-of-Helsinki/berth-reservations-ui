cc527a63e68ec64c1dacb7c50d9d64e0deab0d2e
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-05-27 10:55:43 +0300

    Fixing NabVar links and styles

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Container, Nav, Navbar as BSNavbar, NavbarBrand } from 'reactstrap';
+import { Container, Nav, Navbar as BSNavbar, NavItem } from 'reactstrap';
 
 import Icon from '../../common/Icon';
 import LocalizedLink from '../../common/LocalizedLink';
@@ -14,11 +14,11 @@ const Navbar = () => {
       <div className="vene-navbar__top">
         <Container>
           <BSNavbar expand="md">
-            <NavbarBrand>
-              <LocalizedLink to="/">
-                <Icon className="vene-navbar__icon" name="helsinkiLogo" />
-              </LocalizedLink>
-            </NavbarBrand>
+            {/* <NavItem> */}
+            <LocalizedLink to="/">
+              <Icon className="vene-navbar__icon" name="helsinkiLogo" />
+            </LocalizedLink>
+            {/* </NavItem> */}
             <Nav className="ml-auto" navbar>
               <LanguageDropdown />
             </Nav>
@@ -27,36 +27,32 @@ const Navbar = () => {
       </div>
       <div className="vene-navbar__bottom">
         <Container>
-          <BSNavbar expand="md">
-            <NavbarBrand>
-              <LocalizedLink
-                to="/"
-                className="vene-navbar__link vene-navbar__link--main"
-                activeClassName="vene-navbar__link--active"
-                exact
-              >
-                <FormattedMessage id="site.front.title" />
-              </LocalizedLink>
-            </NavbarBrand>
-            <NavbarBrand>
-              <LocalizedLink
-                to="/berths"
-                className="vene-navbar__link"
-                activeClassName="vene-navbar__link--active"
-              >
-                <FormattedMessage id="site.berth.title" />
-              </LocalizedLink>
-            </NavbarBrand>
-            <NavbarBrand>
-              <LocalizedLink
-                to="/winter_storage"
-                className="vene-navbar__link"
-                activeClassName="vene-navbar__link--active"
-              >
-                <FormattedMessage id="site.winter.title" />
-              </LocalizedLink>
-            </NavbarBrand>
-          </BSNavbar>
+          <Nav expand="md">
+            <LocalizedLink
+              to="/"
+              className="vene-navbar__link vene-navbar__link--main"
+              activeClassName="vene-navbar__link--active"
+              exact
+            >
+              <FormattedMessage id="site.front.title" />
+            </LocalizedLink>
+
+            <LocalizedLink
+              to="/berths"
+              className="vene-navbar__link"
+              activeClassName="vene-navbar__link--active"
+            >
+              <FormattedMessage id="site.berth.title" />
+            </LocalizedLink>
+
+            <LocalizedLink
+              to="/winter_storage"
+              className="vene-navbar__link"
+              activeClassName="vene-navbar__link--active"
+            >
+              <FormattedMessage id="site.winter.title" />
+            </LocalizedLink>
+          </Nav>
         </Container>
       </div>
     </div>
