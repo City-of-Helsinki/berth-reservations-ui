3d2a15d00b7f7545d8745bc8d5330943c628ac99
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-05-31 10:17:20 +0300

    Pass services to the SelectedBerth components

--- src/components/berths/SelectedBerths/index.tsx
+++ src/components/berths/SelectedBerths/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Alert } from 'reactstrap';
+
 import SelectedBerth from '../Berth/SelectedBerth';
 
+import { IconNames } from '../../common/Icon';
 import { Berths } from '../types';
+
+import './selectedBerths.scss';
 
 interface Props {
   berths: Berths;
@@ -13,27 +17,53 @@ interface Props {
   berthValidator: Function;
 }
 
-const SelectedBerths = ({ berths, moveUp, moveDown, deselectBerth, berthValidator }: Props) =>
-  berths.size > 0 ? (
-    <div>
-      {berths.map((berth, index) => (
-        <SelectedBerth
-          index={index}
-          first={index === 0}
-          last={index === berths.size - 1}
-          key={berth.id}
-          berth={berth}
-          moveUp={moveUp}
-          moveDown={moveDown}
-          deselectBerth={deselectBerth}
-          isValid={berthValidator(berth)}
-        />
-      ))}
+const SelectedBerths = ({ berths, moveUp, moveDown, deselectBerth, berthValidator }: Props) => {
+  if (berths.size === 0) {
+    return (
+      <Alert color="danger">
+        <FormattedMessage tagName="strong" id="page.berth.selected.alert.strong" />
+        <FormattedMessage tagName="h2" id="page.berth.selected.alert.paragraph" />
+      </Alert>
+    );
+  }
+
+  return (
+    <div className="vene-selected-berths">
+      {berths.map((berth, index) => {
+        const services: Array<[IconNames, boolean]> =
+          berth.__typename === 'HarborType'
+            ? [
+                ['plug', berth.electricity],
+                ['waterTap', berth.water],
+                ['trash', berth.wasteCollection],
+                ['fence', berth.gate],
+                ['streetLight', berth.lighting]
+              ]
+            : [
+                ['waterTap', berth.water],
+                ['fence', berth.gate],
+                ['plug', berth.electricity],
+                ['dollyEmpty', berth.summerStorageForTrailers],
+                ['trestle', berth.summerStorageForDockingEquipment],
+                ['tools', berth.repairArea]
+              ];
+
+        return (
+          <SelectedBerth
+            className="vene-selected-berths__berth"
+            title={`${index + 1}. ${berth.name}`}
+            key={berth.id}
+            berth={berth}
+            services={services}
+            moveUp={index !== 0 ? moveUp : undefined}
+            moveDown={index !== berths.size - 1 ? moveDown : undefined}
+            handleRemove={deselectBerth}
+            isValid={berthValidator(berth)}
+          />
+        );
+      })}
     </div>
-  ) : (
-    <Alert color="danger">
-      <FormattedMessage tagName="strong" id="page.berth.selected.alert.strong" />
-      <FormattedMessage tagName="h2" id="page.berth.selected.alert.paragraph" />
-    </Alert>
   );
+};
+
 export default SelectedBerths;
