e57a725791a604e10e9aef1268e8e227f2a5f3a6
Chi Nguyen <chi.nguyen@digia.com>
2019-06-03 23:32:57 +0300

    add checkbox field and translation mock for storageType

--- src/components/forms/fragments/UnRegisteredBoatDetails.tsx
+++ src/components/forms/fragments/UnRegisteredBoatDetails.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Col, Row } from 'reactstrap';
 
 import { mustBePositiveNumber } from '../../../utils/formValidation';
-import { Number } from '../Fields';
-import { BoatType, WithBoatType } from '../Selects';
+import { Number, Checkbox } from '../Fields';
+import { BoatType } from '../Selects';
 
 import { BoatTypes } from '../../../types/boatTypes';
 
@@ -14,39 +14,44 @@ interface Props {
   boatTypes?: BoatTypes;
 }
 
-const UnRegisteredBoatDetailsFragment = ({ fieldsNotRequired, boatTypes, hideTitle }: Props) => (
-  <>
-    {!hideTitle && <FormattedMessage tagName="h3" id="form.unregistered.header.title" />}
-    <Row>
-      {boatTypes && (
+const UnRegisteredBoatDetailsFragment = ({ fieldsNotRequired, boatTypes, hideTitle }: Props) => {
+  return (
+    <>
+      {!hideTitle && <FormattedMessage tagName="h3" id="form.unregistered.header.title" />}
+      <Row>
+        {boatTypes && (
+          <Col sm={4}>
+            <BoatType boatTypes={boatTypes} required={!fieldsNotRequired} />
+          </Col>
+        )}
         <Col sm={4}>
-          <BoatType boatTypes={boatTypes} required={!fieldsNotRequired} />
+          <Number
+            validate={mustBePositiveNumber}
+            name={`boatWidth`}
+            label="form.no_boat.field.width.label"
+            placeholder="form.no_boat.field.width.placeholder"
+            append="m"
+            min="0"
+            required={!fieldsNotRequired}
+          />
         </Col>
-      )}
-      <Col sm={4}>
-        <Number
-          validate={mustBePositiveNumber}
-          name={`boatWidth`}
-          label="form.no_boat.field.width.label"
-          placeholder="form.no_boat.field.width.placeholder"
-          append="m"
-          min="0"
-          required={!fieldsNotRequired}
-        />
-      </Col>
-      <Col sm={4}>
-        <Number
-          validate={mustBePositiveNumber}
-          name={`boatLength`}
-          label="form.no_boat.field.length.label"
-          placeholder="form.no_boat.field.length.placeholder"
-          append="m"
-          min="0"
-          required={!fieldsNotRequired}
-        />
-      </Col>
-    </Row>
-  </>
-);
+        <Col sm={4}>
+          <Number
+            validate={mustBePositiveNumber}
+            name={`boatLength`}
+            label="form.no_boat.field.length.label"
+            placeholder="form.no_boat.field.length.placeholder"
+            append="m"
+            min="0"
+            required={!fieldsNotRequired}
+          />
+        </Col>
+        <Col sm={4}>
+          <Checkbox name={`storageType`} label="form.registered.field.winter_storage.storage_type.label" inline="false" />
+        </Col>
+      </Row>
+    </>
+  )
+};
 
 export default UnRegisteredBoatDetailsFragment;
