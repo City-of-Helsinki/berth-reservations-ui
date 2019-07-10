53057baa3a6a65a03a691351a30b79438d3798b7
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-07-10 11:52:18 +0300

    Test setting the HTML lang attribute as the value of locale

--- src/components/layout/languageDropdown/LanguageDropdown.test.tsx
+++ src/components/layout/languageDropdown/LanguageDropdown.test.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Dropdown } from 'reactstrap';
 
-import { shallowWithIntl } from '../../../utils/testUtils';
+import { mountWithIntl } from '../../../utils/testUtils';
 import LanguageDropdown from './LanguageDropdown';
 
-describe('test', () => {
-  const getWrapper = () => shallowWithIntl(<LanguageDropdown />);
+describe('LanguageDropdown', () => {
+  const getWrapper = () => mountWithIntl(<LanguageDropdown />);
 
   test('should render Dropdown component', () => {
     const wrapper = getWrapper();
 
     expect(wrapper.find(Dropdown)).toHaveLength(1);
+  });
+
+  test('should set the HTML lang attribute as the value of locale', () => {
+    const locale = 'fr-ch';
+    const wrapper = getWrapper();
+    const intl = wrapper.prop('intl');
+
+    wrapper.setContext({ intl: { ...intl, locale } });
+
+    expect(document.documentElement.lang).toBe(locale);
   });
 });
