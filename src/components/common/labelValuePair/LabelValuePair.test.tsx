579eb164835fed1ccb20bfe9381b01e5c8d139c1
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-07-31 16:32:05 +0300

    Implement a LabelValuePair component

---
+++ src/components/common/labelValuePair/LabelValuePair.test.tsx
@@ -0,0 +1,24 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import LabelValuePair from './LabelValuePair';
+
+describe('LabelValuePair', () => {
+  const getWrapper = (props: { label: string; value?: string } = { label: 'foo', value: 'bar' }) =>
+    shallow(<LabelValuePair {...props} />);
+
+  test('should render a wrapper with a className of "vene-label-value-pair"', () => {
+    expect(getWrapper().find('div.vene-label-value-pair')).toHaveLength(1);
+  });
+
+  test('should pass the label to FormattedMessage', () => {
+    const label = 'Custom label';
+
+    expect(
+      getWrapper({ label })
+        .find(FormattedMessage)
+        .prop('id')
+    ).toBe(label);
+  });
+});
