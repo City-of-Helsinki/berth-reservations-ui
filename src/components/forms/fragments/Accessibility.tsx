1b82582aa3ae6f2e0abfa242d3f8e21748eb7798
Nizar Rahme <nizar.w.rahmah@gmail.com>
2020-04-24 19:29:51 +0300

    Remove checkbox & add fi, en translations

--- src/components/forms/fragments/Accessibility.tsx
+++ src/components/forms/fragments/Accessibility.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import { Checkbox } from '../Fields';
-
 const AccessibilityFragment = () => (
   <>
     <FormattedMessage tagName="h3" id="form.registered.header.accessibility" />
-    <Checkbox
-      name={`accessibilityRequired`}
-      label="form.accessibility.field.accessibility.label"
-      inline={false}
-    />
+    <FormattedMessage id="form.accessibility.field.accessibility.label" />
   </>
 );
 
