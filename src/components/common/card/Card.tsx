3f6872a7178fd03d1c022a7c50d3d9c750197d46
Santtu Laukkanen <santtu.laukkanen@gmail.com>
2020-06-08 09:09:22 +0300

    Change card title to h3

--- src/components/common/card/Card.tsx
+++ src/components/common/card/Card.tsx
@@ -22,7 +22,9 @@ const Card = ({
   return (
     <RSCard className="vene-card">
       <CardBody className="vene-card__body">
-        <CardTitle className="vene-card__title">{formatMessage({ id: title })}</CardTitle>
+        <CardTitle className="vene-card__title" tag="h3">
+          {formatMessage({ id: title })}
+        </CardTitle>
         <div className="vene-card__description">{children}</div>
         <Button
           onClick={onClick}
