c21fb456ea57b112f0ec01d00afa57089f4eda67
Tatu Arvela <tatu.arvela@nitor.com>
2020-07-06 10:50:07 +0300

    Add payment error and received pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,6 +9,11 @@ import WinterFormPage from './pages/formPage/WinterFormPageContainer';
 import FrontPage from './pages/frontPage/FrontPage';
 import ApplicationThankYouPage from './pages/notice/ApplicationSentPage';
 import NotFoundPage from './pages/notice/NotFoundPage';
+import AlreadyPaidPage from './pages/notice/paymentError/AlreadyPaidPage';
+import CustomerInformationErrorPage from './pages/notice/paymentError/CustomerInformationErrorPage';
+import GeneralErrorPage from './pages/notice/paymentError/GeneralErrorPage';
+import PastDueDatePage from './pages/notice/paymentError/PastDueDatePage';
+import PaymentReceivedPage from './pages/notice/PaymentReceivedPage';
 import SelectedAreaPage from './pages/selectedAreaPage/SelectedAreaPageContainer';
 import SelectedBerthPage from './pages/selectedBerthPage/SelectedBerthPageContainer';
 import WinterStoragePage from './pages/winterStoragePage/WinterStoragePageContainer';
@@ -45,6 +50,15 @@ const App = ({
       <Route exact path={`/${localeParam}/${winterParam}/form/:tab`} component={WinterFormPage} />
 
       <Route exact path={`/${localeParam}/thank-you`} component={ApplicationThankYouPage} />
+      <Route exact path={`/${localeParam}/payment-received`} component={PaymentReceivedPage} />
+      <Route exact path={`/${localeParam}/already-paid`} component={AlreadyPaidPage} />
+      <Route
+        exact
+        path={`/${localeParam}/customer-information-error`}
+        component={CustomerInformationErrorPage}
+      />
+      <Route exact path={`/${localeParam}/general-error`} component={GeneralErrorPage} />
+      <Route exact path={`/${localeParam}/past-due-date`} component={PastDueDatePage} />
       <Route component={NotFoundPage} />
     </Switch>
   </IntlProvider>
---
+++ src/components/pages/notice/PaymentReceivedPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NoticeTemplate from './NoticeTemplate';
+import resetStore from './resetStore';
+
+const PaymentReceivedPage = () => (
+  <NoticeTemplate
+    titleKey="page.payment_received.title"
+    messageKey="page.payment_received.message.paragraph1"
+    secondMessageKey="page.payment_received.message.paragraph2"
+    backgroundColor="fog"
+  />
+);
+
+export default resetStore(PaymentReceivedPage);
