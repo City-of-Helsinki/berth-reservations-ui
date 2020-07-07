1e158331113a2d05c8e459cc93d6446b310c16c1
Tatu Arvela <tatu.arvela@nitor.com>
2020-07-07 11:41:22 +0300

    Make layout fill viewport

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,18 +4,20 @@ import Footer from './footer/Footer';
 import KoroSection from './koroSection/KoroSection';
 import Navbar from './navbar/Navbar';
 
+import './layout.scss';
+
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: Props) => (
-  <>
+  <div className="vene-layout">
     <Navbar />
     {children}
     <KoroSection top color="blue">
       <Footer />
     </KoroSection>
-  </>
+  </div>
 );
 
 export default Layout;
