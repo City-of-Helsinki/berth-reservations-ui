754d530f86f129ad5665aba6c9030aef76bb7861
Nizar Rahme <nizar.w.rahmah@gmail.com>
2020-11-20 12:04:17 +0200

    Customize PaymentPage based on OrderType

--- src/components/pages/paymentPage/PaymentPageContainer.test.tsx
+++ src/components/pages/paymentPage/PaymentPageContainer.test.tsx
@@ -2,28 +2,34 @@ import { shallow } from 'enzyme';
 import React from 'react';
 
 import { getPaymentPage } from './PaymentPageContainer';
-import { OrderStatus } from '../../../__generated__/globalTypes';
+import { OrderStatus, OrderTypeEnum } from '../../../__generated__/globalTypes';
 
 describe('PaymentPageContainer', () => {
+  const t = jest.fn();
   it('should get correct page based on order status', () => {
     const confirmPayment = jest.fn();
 
-    let wrapper = shallow(getPaymentPage(null, confirmPayment));
+    let wrapper = shallow(getPaymentPage(OrderTypeEnum.BERTH, null, confirmPayment, t));
     expect(wrapper.find('#vene-payment-general-error-page').exists()).toBeTruthy();
 
-    wrapper = shallow(getPaymentPage(undefined, confirmPayment));
+    wrapper = shallow(getPaymentPage(OrderTypeEnum.BERTH, undefined, confirmPayment, t));
     expect(wrapper.find('#vene-payment-general-error-page').exists()).toBeTruthy();
 
-    wrapper = shallow(getPaymentPage(OrderStatus.WAITING, confirmPayment));
+    wrapper = shallow(getPaymentPage(OrderTypeEnum.BERTH, OrderStatus.WAITING, confirmPayment, t));
     expect(wrapper.find('#vene-payment-page').exists()).toBeTruthy();
 
-    wrapper = shallow(getPaymentPage(OrderStatus.EXPIRED, confirmPayment));
+    wrapper = shallow(
+      getPaymentPage(OrderTypeEnum.ADDITIONAL_PRODUCT, OrderStatus.WAITING, confirmPayment, t)
+    );
+    expect(wrapper.find('#vene-payment-page').exists()).toBeTruthy();
+
+    wrapper = shallow(getPaymentPage(OrderTypeEnum.BERTH, OrderStatus.EXPIRED, confirmPayment, t));
     expect(wrapper.find('#vene-payment-expired-page').exists()).toBeTruthy();
 
-    wrapper = shallow(getPaymentPage(OrderStatus.PAID, confirmPayment));
+    wrapper = shallow(getPaymentPage(OrderTypeEnum.BERTH, OrderStatus.PAID, confirmPayment, t));
     expect(wrapper.find('#vene-already-paid-page').exists()).toBeTruthy();
 
-    wrapper = shallow(getPaymentPage(OrderStatus.REJECTED, confirmPayment));
+    wrapper = shallow(getPaymentPage(OrderTypeEnum.BERTH, OrderStatus.REJECTED, confirmPayment, t));
     expect(wrapper.find('#vene-payment-general-error-page').exists()).toBeTruthy();
   });
 });
