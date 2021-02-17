22fb651330c1dfd4de185da5862817994c8a111b
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-02-17 16:00:46 +0200

    Add a redirect button to noBerths view

--- src/features/profile/customerBerths/NoBerths.tsx
+++ src/features/profile/customerBerths/NoBerths.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { IconFaceSad } from 'hds-react';
+import { Button, IconFaceSad } from 'hds-react';
 
-const NoBerths = () => {
+import { LocalePush, withMatchParamsHandlers } from '../../../common/utils/container';
+import './noBerths.scss';
+
+interface NoBerthsProps {
+  localePush: LocalePush;
+}
+
+const NoBerths = ({ localePush }: NoBerthsProps) => {
   const { t } = useTranslation();
 
   return (
     <div>
-      <IconFaceSad size="m" aria-hidden="true" /> <strong>{t('page.profile.berths.noBerths')}</strong>
+      <IconFaceSad size="m" aria-hidden="true" /> <strong>{t('page.profile.berths.no_berths')}</strong>
+      <br />
+      <Button className="vene-no-berths__action-btn" onClick={() => localePush('berths')}>
+        {t('page.profile.berths.apply_for_berth')}
+      </Button>
     </div>
   );
 };
 
-export default NoBerths;
+export default withMatchParamsHandlers(NoBerths);
