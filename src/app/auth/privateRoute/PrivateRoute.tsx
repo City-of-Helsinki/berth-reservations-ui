4c3511842cb5b5ec7e4cd47b155485ef62e88667
Tatu Arvela <tatu.arvela@nitor.com>
2021-03-09 16:41:56 +0200

    Localize login in PrivateRoute

--- src/app/auth/privateRoute/PrivateRoute.tsx
+++ src/app/auth/privateRoute/PrivateRoute.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Route, Redirect, RouteProps, useLocation } from 'react-router-dom';
 
+import { localizedLink } from '../../../common/layout/navbar/utils';
 import authService from '../authService';
 
 const PrivateRoute = (props: RouteProps) => {
+  const {
+    i18n: { language },
+  } = useTranslation();
   const location = useLocation();
   const currentLocation = `${location.pathname}${location.search}${location.hash}`;
   const queryString = `?referrer=${encodeURIComponent(currentLocation)}`;
@@ -13,7 +18,7 @@ const PrivateRoute = (props: RouteProps) => {
   return (
     <Redirect
       to={{
-        pathname: `/login${queryString}`,
+        pathname: localizedLink(`/login${queryString}`, language),
       }}
     />
   );
