3fb2b055f5f69d9faa00fd2031282f24efdabed7
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-04-15 14:14:33 +0300

    Fix failing unit tests

--- src/features/payment/paymentPage/__tests__/PaymentPageContainer.test.tsx
+++ src/features/payment/paymentPage/__tests__/PaymentPageContainer.test.tsx
@@ -4,17 +4,33 @@ import { getPaymentPage } from '../PaymentPageContainer';
 import { OrderStatus, OrderTypeEnum } from '../../../../__generated__/globalTypes';
 
 describe('PaymentPageContainer', () => {
+  const placeDetails = {
+    harbor: 'Test Harbor',
+    pier: 'Test Pier',
+    berth: '10',
+  };
+
   it('should get correct page based on order status', () => {
     [
       [null, '#vene-payment-general-error-page'],
       [undefined, '#vene-payment-general-error-page'],
-      [OrderStatus.WAITING, '#vene-payment-page'],
+      [OrderStatus.OFFERED, '#vene-payment-page'],
       [OrderStatus.EXPIRED, '#vene-payment-expired-page'],
       [OrderStatus.PAID, '#vene-already-paid-page'],
       [OrderStatus.REJECTED, '#vene-payment-general-error-page'],
     ].forEach((testItem) => {
       const wrapper = shallow(
-        getPaymentPage(OrderTypeEnum.BERTH, '', true, testItem[0] as OrderStatus, [], jest.fn(), jest.fn(), jest.fn())
+        getPaymentPage(
+          placeDetails,
+          OrderTypeEnum.BERTH,
+          '',
+          true,
+          testItem[0] as OrderStatus,
+          [],
+          jest.fn(),
+          jest.fn(),
+          jest.fn()
+        )
       );
       expect(wrapper.find(testItem[1] as string).exists()).toBeTruthy();
     });
