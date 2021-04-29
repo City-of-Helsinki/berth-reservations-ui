d240126b79613674f96ba3dc0e3be9b78b46b128
Tatu Arvela <tatu.arvela@nitor.com>
2021-04-29 16:43:46 +0300

    Update features/berth/utils.test.ts

--- src/features/berth/__tests__/utils.test.ts
+++ src/features/berth/__tests__/utils.test.ts
@@ -1,36 +1,11 @@
 import { Record } from 'immutable';
 
-import { berth, createBerth } from '../../../__fixtures__/berthFixture';
+import { harbor, createHarbor } from '../../../__fixtures__/harborFixture';
 import { createBerthValues } from '../../../__fixtures__/formValuesFixture';
 import { SelectedServicesProps } from '../../../common/types/services';
-import { mockPiers } from '../__fixtures__/mockData';
-import { anyPierHasProperty, getBerthFilterByValues, getSuitableBoatTypes, onlyUnique } from '../utils';
+import { getBerthFilterByValues } from '../utils';
 
 describe('berth utils', () => {
-  describe('anyPierHasProperty', () => {
-    test('should return correct information', () => {
-      expect(anyPierHasProperty(mockPiers, 'electricity')).toBe(false);
-      expect(anyPierHasProperty(mockPiers, 'gate')).toBe(true);
-      expect(anyPierHasProperty(mockPiers, 'lighting')).toBe(true);
-      expect(anyPierHasProperty(mockPiers, 'mooring')).toBe(true);
-      expect(anyPierHasProperty(mockPiers, 'wasteCollection')).toBe(false);
-      expect(anyPierHasProperty(mockPiers, 'water')).toBe(true);
-    });
-  });
-
-  describe('onlyUnique', () => {
-    test('should filter entries and return only unique entries', () => {
-      const testData = ['1', '1', '2', '3', '4', '1'];
-      expect(testData.filter(onlyUnique)).toEqual(['1', '2', '3', '4']);
-    });
-  });
-
-  describe('getSuitableBoatTypes', () => {
-    test('should return all suitable boat types from a list of piers', () => {
-      expect(getSuitableBoatTypes(mockPiers)).toEqual(['1', '2', '3', '5']);
-    });
-  });
-
   describe('getBerthFilterByValues', () => {
     const defaultValues = createBerthValues({
       boatLength: '10.2',
@@ -63,12 +38,12 @@ describe('berth utils', () => {
       const returnValue = getBerthFilterByValues(defaultValues, createSelectedServices());
 
       expect(typeof returnValue).toBe('function');
-      expect(typeof returnValue(createBerth())).toBe('boolean');
-      expect(typeof returnValue(berth)).toBe('boolean');
+      expect(typeof returnValue(createHarbor())).toBe('boolean');
+      expect(typeof returnValue(harbor)).toBe('boolean');
     });
 
     test('should return true if all filter conditions are met', () => {
-      const value = getBerthFilterByValues(defaultValues, createSelectedServices())(createBerth(matchOptions));
+      const value = getBerthFilterByValues(defaultValues, createSelectedServices())(createHarbor(matchOptions));
 
       expect(value).toBe(true);
     });
@@ -78,27 +53,27 @@ describe('berth utils', () => {
       const value = getBerthFilterByValues(
         defaultValues,
         createSelectedServices({ gate: true })
-      )(createBerth(customOptions));
+      )(createHarbor(customOptions));
 
       expect(value).toBe(false);
     });
 
     test('should return false if the supplied width is larger than maxWidth after converting to cm', () => {
-      const customOptions = createBerthOptions({ maxWidth: 490 });
+      const customOptions = createBerthOptions({ maxWidth: 4.9 });
       const value = getBerthFilterByValues(
         createBerthValues({ boatWidth: '5' }),
         createSelectedServices()
-      )(createBerth(customOptions));
+      )(createHarbor(customOptions));
 
       expect(value).toBe(false);
     });
 
     test('should return false if the supplied length is larger than maxLength after converting to cm', () => {
-      const customOptions = createBerthOptions({ maxLength: 900 });
+      const customOptions = createBerthOptions({ maxLength: 9.0 });
       const value = getBerthFilterByValues(
         createBerthValues({ boatLength: '10' }),
         createSelectedServices()
-      )(createBerth(customOptions));
+      )(createHarbor(customOptions));
 
       expect(value).toBe(false);
     });
@@ -107,7 +82,7 @@ describe('berth utils', () => {
       const berthOptions = createBerthOptions({
         suitableBoatTypes: [{ __typename: 'BoatTypeType', id: '19' }],
       });
-      const newBerth = createBerth(berthOptions);
+      const newBerth = createHarbor(berthOptions);
       const value = getBerthFilterByValues(createBerthValues({ boatType: '10' }), createSelectedServices())(newBerth);
 
       expect(value).toBe(false);
