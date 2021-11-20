544a5d2f17779ba1d9da77308233bc3e0efa55bb
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-11-20 12:25:46 +0200

    Remove berthSwitch state from Redux store

--- src/features/notice/resetStore.tsx
+++ src/features/notice/resetStore.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { compose, lifecycle } from 'recompose';
 
-import { resetBerthSwitch } from '../../redux/actions/BerthSwitchActions';
 import { resetBerths } from '../../redux/actions/BerthActions';
 import { resetValues } from '../../redux/actions/FormActions';
 import { resetWinterAreas } from '../../redux/actions/WinterAreaActions';
@@ -11,24 +10,21 @@ interface Props {
   resetValues: () => void;
   resetBerths: () => void;
   resetWinterAreas: () => void;
-  resetBerthSwitch: () => void;
 }
 
 const resetStore = (component: React.ComponentType<Props>) =>
   compose<Props, {}>(
-    connect(() => ({}), { resetValues, resetBerths, resetWinterAreas, resetBerthSwitch }),
+    connect(() => ({}), { resetValues, resetBerths, resetWinterAreas }),
     lifecycle<Props, {}>({
       componentDidMount() {
         const {
           resetValues: resetForm,
           resetBerths: resetBerthsState,
           resetWinterAreas: resetWinterStorageState,
-          resetBerthSwitch: resetBerthSwitchState,
         } = this.props;
         resetForm();
         resetBerthsState();
         resetWinterStorageState();
-        resetBerthSwitchState();
       },
     })
   )(component);
