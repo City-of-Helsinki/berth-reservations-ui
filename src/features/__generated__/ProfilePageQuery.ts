3765da5657a6d0dc37023650db79f395c4064980
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-11-22 16:37:11 +0200

    Use double ticks as the script generates them

--- src/features/__generated__/ProfilePageQuery.ts
+++ src/features/__generated__/ProfilePageQuery.ts
@@ -3,14 +3,14 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { Language } from './../../__generated__/globalTypes';
+import { Language } from "./../../__generated__/globalTypes";
 
 // ====================================================
 // GraphQL query operation: ProfilePageQuery
 // ====================================================
 
 export interface ProfilePageQuery_myProfile_primaryAddress {
-  __typename: 'AddressNode';
+  __typename: "AddressNode";
   id: string;
   address: string;
   postalCode: string;
@@ -18,65 +18,65 @@ export interface ProfilePageQuery_myProfile_primaryAddress {
 }
 
 export interface ProfilePageQuery_myProfile_primaryEmail {
-  __typename: 'EmailNode';
+  __typename: "EmailNode";
   id: string;
   email: string;
 }
 
 export interface ProfilePageQuery_myProfile_primaryPhone {
-  __typename: 'PhoneNode';
+  __typename: "PhoneNode";
   id: string;
   phone: string;
 }
 
 export interface ProfilePageQuery_myProfile_berthLeases_edges_node_berth_pier_properties_harbor_properties {
-  __typename: 'HarborProperties';
+  __typename: "HarborProperties";
   name: string | null;
 }
 
 export interface ProfilePageQuery_myProfile_berthLeases_edges_node_berth_pier_properties_harbor {
-  __typename: 'HarborNode';
+  __typename: "HarborNode";
   id: string;
   properties: ProfilePageQuery_myProfile_berthLeases_edges_node_berth_pier_properties_harbor_properties | null;
 }
 
 export interface ProfilePageQuery_myProfile_berthLeases_edges_node_berth_pier_properties {
-  __typename: 'PierProperties';
+  __typename: "PierProperties";
   identifier: string;
   harbor: ProfilePageQuery_myProfile_berthLeases_edges_node_berth_pier_properties_harbor;
 }
 
 export interface ProfilePageQuery_myProfile_berthLeases_edges_node_berth_pier {
-  __typename: 'PierNode';
+  __typename: "PierNode";
   id: string;
   properties: ProfilePageQuery_myProfile_berthLeases_edges_node_berth_pier_properties | null;
 }
 
 export interface ProfilePageQuery_myProfile_berthLeases_edges_node_berth {
-  __typename: 'BerthNode';
+  __typename: "BerthNode";
   id: string;
   number: string;
   pier: ProfilePageQuery_myProfile_berthLeases_edges_node_berth_pier;
 }
 
 export interface ProfilePageQuery_myProfile_berthLeases_edges_node {
-  __typename: 'BerthLeaseNode';
+  __typename: "BerthLeaseNode";
   id: string;
   berth: ProfilePageQuery_myProfile_berthLeases_edges_node_berth;
 }
 
 export interface ProfilePageQuery_myProfile_berthLeases_edges {
-  __typename: 'BerthLeaseNodeEdge';
+  __typename: "BerthLeaseNodeEdge";
   node: ProfilePageQuery_myProfile_berthLeases_edges_node | null;
 }
 
 export interface ProfilePageQuery_myProfile_berthLeases {
-  __typename: 'BerthLeaseNodeConnection';
+  __typename: "BerthLeaseNodeConnection";
   edges: (ProfilePageQuery_myProfile_berthLeases_edges | null)[];
 }
 
 export interface ProfilePageQuery_myProfile {
-  __typename: 'ProfileNode';
+  __typename: "ProfileNode";
   id: string;
   language: Language | null;
   firstName: string;
