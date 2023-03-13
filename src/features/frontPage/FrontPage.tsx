82e5bc62315b92835f73ffc1038a4c468a16e2e4
Kristian <53874567+kristianjokela@users.noreply.github.com>
2023-03-13 14:41:55 +0200

    hide 2022 trailer payment link from frontpage

--- src/features/frontPage/FrontPage.tsx
+++ src/features/frontPage/FrontPage.tsx
@@ -89,6 +89,7 @@ const FrontPage = ({ localePush }: Props) => {
                 title={t('page.front.card.trailer.title')}
                 href={trailerPaymentURL.href}
                 rel="noopener noreferrer nofollow"
+                buttonHidden
               >
                 <p>{t('page.front.card.trailer.description')}</p>
               </Card>
