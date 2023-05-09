6274f78928672af7ab11ce26aa9d974f1fc3b8cc
Ivan Melnik <melnik.job@gmail.com>
2023-05-09 09:31:33 +0300

    fix terms

--- src/features/terms/TermsOfService.tsx
+++ src/features/terms/TermsOfService.tsx
@@ -7,40 +7,25 @@ const TermsOfService = () => {
 
       <h3 className="vene-terms-of-service__sub-heading">PALVELUN MAKSAMINEN JA KÄYTTÖ</h3>
       <p>
-        Varattu trailerin kesäsäilytyspaikka on asiakkaan käytettävissä sopimuksessa määriteltynä aikana. Asiakkaalla
-        tulee olla voimassa oleva venepaikan vuokrasopimus Helsingin kaupungin kanssa. Trailerin kesäsäilytys tällä
-        maksulla on mahdollista merkityillä trailerin kesäsäilytysalueilla purjehduskaudella 10.6.-14.9.2021. Traileria
-        voi säilyttää alueella siihen asti, kunnes nostaa veneensä vedestä talvisäilytystä varten.
+      Varattu trailerin kesäsäilytyspaikka on asiakkaan käytettävissä sopimuksessa määriteltynä aikana. Asiakkaalla tulee olla voimassa oleva venepaikan vuokrasopimus Helsingin kaupungin kanssa. Trailerin kesäsäilytys tällä maksulla on mahdollista merkityillä trailerin kesäsäilytysalueillapurjehduskaudella 10.6.-14.9. maksusuoritusta vastaavana vuonna.Trailerin voi kuitenkin viedä alueelle aikaisemmin, jos alueilla on tilaaja säilyttää alueella siihen asti, kunnes nostaa veneensä vedestä talvisäilytystä varten.
       </p>
 
       <h3 className="vene-terms-of-service__sub-heading">MAKSAMINEN</h3>
       <p>
-        Trailerin kesäsäilytys maksetaan välittömästi ja viimeistään silloin, kun vene nostetaan vedestä talvisäilytystä
-        varten. Maksu täytyy suorittaa kokonaisuudessaan 31.12.2021 mennessä. Palvelun hinta sisältää arvonlisäveron.
-        Mahdolliset lisäpalvelut eivät sisälly hintaan.
+      Trailerin kesäsäilytys maksetaan välittömästi trailerin alueelle jättämisen jälkeen.Palvelun hinta sisältää arvonlisäveron. Mahdolliset lisäpalvelut eivät sisälly hintaan.
       </p>
 
       <h3 className="vene-terms-of-service__sub-heading">PERUUTUSOIKEUS</h3>
-      <p>Maksu on sitova. Maksua ei palauteta käyttämättä jääneen palvelun perusteella.</p>
+      <p>Palvelu katsotaan otetuksi käyttöön koko purjehduskaudelle,sillä hetkellä, kun traileri on viety säilytysalueelle. Maksua tai sen osaa ei palauteta, vaikka asiakas lopettaisi palvelun käytön ennen purjehduskauden päättymistä.</p>
 
       <h3 className="vene-terms-of-service__sub-heading">ASIAKKAAN VASTUU</h3>
       <p>
-        Asiakas on vastuussa toimintansa turvallisuudesta. Asiakkaalle voidaan antaa velvoittavia palvelun järjestykseen
-        ja ylläpitoon liittyviä tarkempia käyttö- ja turvallisuusohjeita paikallisesti. Asiakas sitoutuu noudattamaan
-        venesatamasääntöjä.
+      Asiakas on vastuussa toimintansa turvallisuudesta. Asiakkaalle voidaan antaa velvoittavia palvelun järjestykseen ja ylläpitoon liittyviä tarkempia käyttö-ja turvallisuusohjeita paikallisesti. Asiakas sitoutuu noudattamaan venesatamasääntöjä.
       </p>
 
       <h3 className="vene-terms-of-service__sub-heading">PALVELUTARJOAJAN PERUUTUSOIKEUS JA SOPIMUKSEN PURKAMINEN</h3>
       <p>
-        Jos palvelutarjoaja ei pysty ylivoimaisen esteen (force majeure) takia tarjoamaan varattua palvelua,
-        palvelutarjoajalla on oikeus peruttaa asiakkaan trailerin kesäsäilytys. Palvelutarjoajan käyttäessä
-        peruutusoikeuttaan asiakkaasta riippumattomista syistä, maksu palautetaan asiakkaalle kokonaisuudessaan.
-        <br />
-        Palvelutarjoajalla on oikeus purkaa sopimus päättymään välittömästi, jos asiakas tai hänen seuralaisensa
-        syyllistyy sellaiseen laiminlyöntiin tai menettelyyn, mikä olennaisesti rikkoo tätä sopimusta, satamakiinteistön
-        järjestyssääntöjä tai venesatamasääntöjä tai suomen lakia, tai jos asiakas tai hänen seuralaisensa harjoittaa
-        satamassa tai trailerin kesäsäilytysalueella rikollista, vaarallista tai muutoin sopimuksen vastaista tai
-        sopimattomaksi katsottavaa toimintaa.
+      Jos palvelutarjoaja ei pysty ylivoimaisen esteen (force majeure) takia tarjoamaan varattua palvelua, palvelutarjoajalla on oikeus peruttaa asiakkaan trailerin kesäsäilytys. Palvelutarjoajan käyttäessä peruutusoikeuttaan asiakkaasta riippumattomista syistä, maksu palautetaan asiakkaalle kokonaisuudessaan.Palvelutarjoajalla on oikeus purkaa sopimus päättymään välittömästi, jos asiakas tai hänen seuralaisensa syyllistyy sellaiseen laiminlyöntiintai menettelyyn, mikä olennaisesti rikkoo tätä sopimusta, satamakiinteistön järjestyssääntöjä tai venesatamasääntöjä tai Suomen lakia, tai jos asiakas tai hänen seuralaisensa harjoittaa satamassa tai trailerin kesäsäilytysalueella rikollista, vaarallista tai muutoin sopimuksen vastaista tai sopimattomaksi katsottavaa toimintaa.Jos trailerin kesäsäilytys joudutaan keskeyttämään asiakkaan häiriökäyttäytymisen tai laiminlyönnin takia, maksuja ei palauteta.
         <br />
         Jos trailerin kesäsäilytys joudutaan keskeyttämään asiakkaan häiriökäyttäytymisen tai laiminlyönnin takia,
         maksuja ei palauteta.
@@ -48,63 +33,24 @@ const TermsOfService = () => {
 
       <h3 className="vene-terms-of-service__sub-heading">VASTUUNRAJOITUKSET JA ERIMIELISYYKSIEN RATKAISEMINEN</h3>
       <p>
-        Palveluntarjoaja ei vastaa peruuntumisesta tai keskeytymisestä aiheutuneista välillisistä vahingoista.
-        Palveluntarjoaja ei vastaa virheestä tai viivästyksestä, jos se aiheutuu ylivoimaisesta esteestä.
+      Palveluntarjoaja ei vastaa peruuntumisesta tai keskeytymisestä aiheutuneista välillisistä vahingoista. Palveluntarjoaja ei vastaa virheestä tai viivästyksestä, jos se aiheutuu ylivoimaisesta esteestä.
         <br />
-        Tätä sopimusta koskevat erimielisyydet ratkaistaan Helsingin käräjäoikeudessa. Kuluttaja-asiakas voi saattaa
-        asian myös kuluttajariitalautakunnan käsiteltäväksi. Sopimukseen sovelletaan Suomen lakia.
+        Tätä sopimusta koskevat erimielisyydet ratkaistaan Helsingin käräjäoikeudessa. Kuluttaja-asiakas voi saattaa asian myös kuluttajariitalautakunnan käsiteltäväksi. Sopimukseen sovelletaan Suomen lakia.
       </p>
 
       <h3 className="vene-terms-of-service__sub-heading">SOPIMUKSEN VOIMAATULO</h3>
       <p>
-        Maksamalla trailerin kesäsäilytyksen asiakas sitoutuu noudattamaan näitä sopimusehtoja sekä venesatamasääntöjä.
-        Asiakas vastaa siitä, että myös hänen seuralaisensa noudattavat tämän sopimuksen ehtoja sekä venesatamasääntöjä.
-        Helsingissä 9/2021.
+      Maksamalla trailerin kesäsäilytyksen asiakas sitoutuu noudattamaan näitä sopimusehtoja sekä venesatamasääntöjä. Asiakas vastaa siitä, että myös hänen seuralaisensa noudattavat tämän sopimuksen ehtoja sekä venesatamasääntöjä.
       </p>
 
-      <h3 className="vene-terms-of-service__sub-heading">MAKSAMINEN</h3>
+      <h3 className="vene-terms-of-service__sub-heading">MAKSAMINEN JA MAKSUTAVAT</h3>
       <p>
-        Hinta sisältää arvolisäveron.
+        Maksamiseen liittyvät tiedot löytyvät osoitteesta https://checkout.hel.fi/fi/information
         <br />
-        Verkkokaupan maksuvälittäjänä toimii Visma Pay (Paybyway Oy, y-tunnus 2486559-4), joka on rekisteröity
-        Finanssivalvonnan ylläpitämään maksulaitosrekisteriin. Maksamiseen siirrytään Visma Payn verkkopalvelun kautta
-        ja tiliotteella ja laskulla maksun saajana näkyy Visma Pay tai Paybyway Oy. Visma Pay välittää maksut
-        verkkokauppiaalle. Maksaminen on turvallista, sillä kaikki maksutapahtumaa koskevat tiedot välitetään salattua
-        yhteyttä käyttäen niin, ettei kukaan ulkopuolinen taho näe maksutapahtuman tietoja.
-        <br />
-        Kauppa syntyy verkkokaupan asiakkaan ja verkkokaupan välille. Verkkokaupan vastuulla ovat kaikki kauppaan
-        liittyvät velvoitteet.
-        <br />
-        Lue lisää Visma Paysta: https://www.visma.fi/vismapay/
+        Verkkosivusto (checkout.hel.fi) on Helsingin kaupunginTaloushallintopalveluliikelaitoksen (Talpan)tuottaman verkkokauppapalvelun kassa, jota kaupungin toimialat ja palvelut voivat käyttää osana omaa verkkokauppaansa. 
       </p>
-
-      <h3 className="vene-terms-of-service__sub-heading">MAKSUTAVAT</h3>
-      <p>
-        Visma Pay -palvelun kautta voit maksaa verkkopankkitunnuksilla, lompakolla, maksukorteilla (credit/debit),
-        laskulla tai osamaksulla. Käytettävissä ovat seuraavat maksutavat: Osuuspankki, Nordea, Danske Bank, Oma
-        Säästöpankki, Säästöpankki, Aktia, Paikallisosuuspankit, S-Pankki, Handelsbanken, Ålandsbanken, MobilePay,
-        Masterpass, Pivo, Visa-, Visa Debit-, Visa Electron-, MasterCard- ja Debit MasterCard - kortit, sekä Jousto ja
-        Enterpay Lasku yritykselle.
-        <br />
-        MobilePay: Voit maksaa MobilePay-lompakollasi mikäli olet sallinut verkkokaupoissa maksamisen sovelluksen
-        asetuksista. MobilePay-lompakolla suoritetut maksut veloitetaan lompakkoon liitetyltä maksukortilta. Mikäli
-        maksun veloittaminen maksukortilta epäonnistuu, MobilePay-lompakolla maksaminen ei ole mahdollista
-        verkkokaupassa.
-        <br />
-        Pivo: Käyttöehdot ovat tarjolla Pivon sivuilla: https://pivo.fi/kayttoehdot/pivon- kayttoehdot/
-        <br />
-        Visma Pay -maksupalvelun yhteystiedot
-        <br />
-        Visma Pay, Paybyway Oy (Y-tunnus 2486559-4) Sähköposti: helpdesk@vismapay.com
-        <br />
-        Puhelin: 09 315 42 037 (arkisin klo 8-16) Postiosoite: Laserkatu 6, 53850 Lappeenranta
-      </p>
-
       <h3 className="vene-terms-of-service__sub-heading">MAKSUVAHVISTUS</h3>
-      <p>Saat automaattisen sähköpostiviestin onnistuneesta maksusta.</p>
-
-      <h3 className="vene-terms-of-service__sub-heading">PERUUTUS</h3>
-      <p>Maksua ei palauteta käyttämättä jääneen trailerin kesäsäilytyksen perusteella.</p>
+      <p>Saat onnistuneesti maksetusta tilauksesta sähköpostitse kuitin sekä vahvistuksen maksusta. Mikäli sähköpostiviestiä ei tule noin 15 minuutin sisällä onnistuneesta maksusta, tarkista sähköpostisi roskapostikansio.</p>
     </div>
   );
 };
