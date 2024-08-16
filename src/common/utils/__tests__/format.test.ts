f1dafe0cc356ba9fdf6b0cdc84dbb01200f7d739
Kari Salminen <kari.salminen@anders.com>
2024-08-16 14:53:13 +0300

    feat: make formatPercentage support 0–3 fractional digits for e.g. 25,5%
    e.g. previously formatPercentage(25.5, 'en') == '26%', now '25.5%'
    
    also add more tests for formatPercentage
    
    refs VEN-1575 (VAT percentage change to 25.5% as of 2024-09-01)

--- src/common/utils/__tests__/format.test.ts
+++ src/common/utils/__tests__/format.test.ts
@@ -56,8 +56,43 @@ describe('format', () => {
       expect(formatPercentage(1, 'fi')).toMatch('%');
     });
 
-    it('should return the right value', () => {
-      expect(formatPercentage(10, 'fi')).toEqual('10 %');
+    // \u00A0 = No-Break Space (NBSP)
+    it.each<[number, string, string]>([
+      // Finnish
+      [0, 'fi', '0\u00A0%'],
+      [1, 'fi', '1\u00A0%'],
+      [1.23, 'fi', '1,23\u00A0%'],
+      [9, 'fi', '9\u00A0%'],
+      [12.3456789, 'fi', '12,346\u00A0%'],
+      [15, 'fi', '15\u00A0%'],
+      [24, 'fi', '24\u00A0%'],
+      [25.5, 'fi', '25,5\u00A0%'],
+      [31.26, 'fi', '31,26\u00A0%'],
+      [42.347, 'fi', '42,347\u00A0%'],
+      // Swedish
+      [0, 'sv', '0\u00A0%'],
+      [1, 'sv', '1\u00A0%'],
+      [1.23, 'sv', '1,23\u00A0%'],
+      [9, 'sv', '9\u00A0%'],
+      [12.3456789, 'sv', '12,346\u00A0%'],
+      [15, 'sv', '15\u00A0%'],
+      [24, 'sv', '24\u00A0%'],
+      [25.5, 'sv', '25,5\u00A0%'],
+      [31.26, 'sv', '31,26\u00A0%'],
+      [42.347, 'sv', '42,347\u00A0%'],
+      // English
+      [0, 'en', '0%'],
+      [1, 'en', '1%'],
+      [1.23, 'en', '1.23%'],
+      [9, 'en', '9%'],
+      [12.3456789, 'en', '12.346%'],
+      [15, 'en', '15%'],
+      [24, 'en', '24%'],
+      [25.5, 'en', '25.5%'],
+      [31.26, 'en', '31.26%'],
+      [42.347, 'en', '42.347%'],
+    ])('formatPercentage(%s, "%s") == "%s"', (value: number, locale: string, expectedResult: string) => {
+      expect(formatPercentage(value, locale)).toEqual(expectedResult);
     });
   });
 });
