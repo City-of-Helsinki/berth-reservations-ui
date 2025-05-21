33d9bf753a4836a5cfd4d9305046816715e4de83
Niko Lindroos <niko.j.makela@gmail.com>
2025-05-21 12:57:21 +0300

    feat: remove routes for winter and unmarked storages
    VEN-1596.
    
    The berth service is shutting down, because it is getting replaced by
    another one. This commit is the first step in a phased shutdown and will
    remove features that are no longer needed or desired in the final
    summer.
    
    > "The payment link should be left for a summer storage of a trailer on
    the site for the summer 2025, since this is a direct link to Talpa's
    online store, which does not interact with the berth reservation
    system."
    
    > "Searching for winter storages, as well as notifications
    for lifting order areas, should be removed."
    
    > "The register statement should be renamed to privacy statement."

--- browser-tests/selectors/frontPage.ts
+++ browser-tests/selectors/frontPage.ts
@@ -6,6 +6,4 @@ const content = Selector('div[class="vene-front-page container"]');
 export const frontPageSelectors = {
   title: Selector('h1[class="vene-hero__title"]'),
   berths: within(content).getByText('Selaa ja hae venepaikkoja'),
-  winterStorage: within(content).getByText('Selaa ja hae talvisäilytyspaikkoja'),
-  unmarkedWinterStorage: within(content).getByText('Ilmoita'),
 };
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,12 +19,7 @@ import OrderCancelledPage from '../features/payment/cancelOrderPage/OrderCancell
 import PaymentPageContainer from '../features/payment/paymentPage/PaymentPageContainer';
 import PrivateRoute from './auth/privateRoute/PrivateRoute';
 import MockProfilePageContainer from '../features/profile/MockProfilePageContainer';
-import SelectedAreaPageContainer from '../features/winterStorage/selectedAreaPage/SelectedAreaPageContainer';
 import SelectedBerthPageContainer from '../features/berth/selectedBerthPage/SelectedBerthPageContainer';
-import UnmarkedWinterFormPageContainer from '../features/unmarkedWinterStorage/unmarkedWinterFormPage/UnmarkedWinterFormPageContainer';
-import UnmarkedWinterStoragePageContainer from '../features/unmarkedWinterStorage/unmarkedWinterStoragePage/UnmarkedWinterStoragePageContainer';
-import WinterFormPageContainer from '../features/winterStorage/winterFormPage/WinterFormPageContainer';
-import WinterStoragePageContainer from '../features/winterStorage/winterStoragePage/WinterStoragePageContainer';
 import i18n from '../locales/i18n';
 import { ApplicationType } from '../common/types/applicationType';
 import { LocaleOpts } from '../common/types/intl';
@@ -35,8 +30,6 @@ type Props = RouteComponentProps<{ locale: LocaleOpts }>;
 
 const localeParam = ':locale(fi|en|sv)';
 const berthParam = `:app(${ApplicationType.BerthApp})`;
-const winterParam = `:app(${ApplicationType.WinterStorageApp})`;
-const unmarkedWsParam = `:app(${ApplicationType.UnmarkedWinterStorageApp})`;
 
 const App = ({
   match: {
@@ -63,15 +56,6 @@ const App = ({
       <Route exact path={`/${localeParam}/${berthParam}/selected`} component={SelectedBerthPageContainer} />
       <Route exact path={`/${localeParam}/${berthParam}/form`} component={BerthFormPageContainer} />
       <Route exact path={`/${localeParam}/${berthParam}/form/:tab`} component={BerthFormPageContainer} />
-
-      <Route exact path={`/${localeParam}/${winterParam}`} component={WinterStoragePageContainer} />
-      <Route exact path={`/${localeParam}/${winterParam}/selected`} component={SelectedAreaPageContainer} />
-      <Route exact path={`/${localeParam}/${winterParam}/form`} component={WinterFormPageContainer} />
-      <Route exact path={`/${localeParam}/${winterParam}/form/:tab`} component={WinterFormPageContainer} />
-
-      <Route exact path={`/${localeParam}/${unmarkedWsParam}`} component={UnmarkedWinterStoragePageContainer} />
-      <Route exact path={`/${localeParam}/${unmarkedWsParam}/form`} component={UnmarkedWinterFormPageContainer} />
-      <Route exact path={`/${localeParam}/${unmarkedWsParam}/form/:tab`} component={UnmarkedWinterFormPageContainer} />
 
       <Route exact path={`/${localeParam}/payment`} component={PaymentPageContainer} />
       <Route exact path={`/${localeParam}/payment-result`} component={PaymentResultContainer} />
